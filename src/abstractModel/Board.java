e542e933a7bc1dd083d3f8b344e48ba2d9090135
matthias.moosburger <matthias.moosburger@d5b95618-209e-438a-8481-01c1a498ce88>
2016-05-17 16:10:10 +0000

    generated framework of abstract Board class

--- src/abstractModel/Board.java
+++ src/abstractModel/Board.java
@@ -1,20 +1,91 @@
 package abstractModel;
 
-import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
 
 public abstract class Board implements Observable {
 
-    @Override
-    public void addListener(InvalidationListener arg0) {
-	// TODO Auto-generated method stub
+	private Edge[] edges;
+	private Corner[] corners;
+	private Field[] fields;
+	//private PlayerModel[] players;
+	private Field bandit;
+	//private DevDeck devDeck;
 	
-    }
+	
+	
+	
+	
+	public Edge[] getEdges() {
+		return edges;
+	}
 
-    @Override
-    public void removeListener(InvalidationListener arg0) {
-	// TODO Auto-generated method stub
+
+
+
+
+	public void setEdges(Edge[] edges) {
+		this.edges = edges;
+	}
+
+
+
+
+
+	public Corner[] getCorners() {
+		return corners;
+	}
+
+
+
+
+
+	public void setCorners(Corner[] corners) {
+		this.corners = corners;
+	}
+
+
+
+
+
+	public Field[] getFields() {
+		return fields;
+	}
+
+
+
+
+
+	public void setFields(Field[] fields) {
+		this.fields = fields;
+	}
+
+
+
+
+
+	public Field getBandit() {
+		return bandit;
+	}
+
+
+
+
+
+	public void setBandit(Field bandit) {
+		this.bandit = bandit;
+	}
+
+
+
+
+
+	/**
+	 * @author mattmoos
+	 * Auxiliary class for navigating hexagonal game board
+	 */
+	private class HexServices{
+		
+	}
 	
-    }
     
 }
