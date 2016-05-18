2fee7fe16c533b1ad4ce6dcb36e698ff083abc52
matthias.moosburger <matthias.moosburger@d5b95618-209e-438a-8481-01c1a498ce88>
2016-05-18 13:20:59 +0000

    view Interface, view and viewControllerInterface created

---
+++ src/client/view/View.java
@@ -0,0 +1,108 @@
+package client.view;
+
+import model.Corner;
+import model.Edge;
+import model.Field;
+
+public class View implements ViewInterface {
+
+	// jeweils die letzte Dimension des Arrays zur Speicherung der Koordinaten;
+	// für Edge 2 Koordinaten (4 Punkte), weil Anfangs- und Endpunkt
+	public int[][][] fieldCoordinates; // [6][6][2]
+	public int[][][][] edgeCoordinates; // [6][6][3][4]
+	public int[][][][] cornerCoordinates; // [6][6][2][2]
+	public static double radius;
+	public static int[] windowCenter; //[2]
+	public static double sin60 = Math.sqrt(3)/2; // Hilfsvariable sqrt(3)/2
+
+	@Override
+	public boolean initialize() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double[] createHexagon(double[] center) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void drawHexagon(double[] points) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void drawChips(double[] centerPoints, int[] diceIndex) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void drawVillage() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void drawStreet() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void drawCity() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void drawDices() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void drawBandit() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void fillFields(Field[] fields) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setVillage(Corner corner) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setStreet(Edge edge) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setCity(Corner corner) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setBandid(Field field) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void calculateFieldCenters() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/client/view/ViewInterface.java
@@ -0,0 +1,54 @@
+package client.view;
+
+import model.Corner;
+import model.Edge;
+import model.Field;
+
+
+
+public interface ViewInterface {
+	public boolean initialize();
+
+	public double[] createHexagon(double[] center);
+
+	public void drawHexagon(double[] points);
+
+	public void drawChips(double[] centerPoints, int[] diceIndex);
+
+	public void drawVillage();
+
+	public void drawStreet();
+
+	public void drawCity();
+
+	public void drawDices();
+
+	public void drawBandit();
+
+	/**
+	 * @param fields
+	 *            TODO: aus jedem Field den RessourceType holen und entsprechend
+	 *            zeichnen
+	 */
+	public void fillFields(Field[] fields);
+
+	public void setVillage(Corner corner);
+
+	public void setStreet(Edge edge);
+
+	/**
+	 * @param corner
+	 *            TODO: vorher village hier auf der corner löschen
+	 */
+	public void setCity(Corner corner);
+	
+	/**
+	 * @param field
+	 * TODO: Bandit an alter Position vorher löschen
+	 */
+	public void setBandid(Field field);
+	
+	public void calculateFieldCenters();
+	
+	
+}
