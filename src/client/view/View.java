f7941d850e95b04d42a21b112ce7949ecab131d9
amina.dacikj <amina.dacikj@d5b95618-209e-438a-8481-01c1a498ce88>
2016-05-19 20:43:44 +0000

    partly implemented Method calculateFieldCenters

--- src/client/view/View.java
+++ src/client/view/View.java
@@ -171,6 +171,80 @@ public class View implements ViewInterface {
 		 * ausgehend von windowCenter = [x,y] die Mittelpunkte aller 43 Felder
 		 * in fieldcoordinates speichern 0,0 -> 3,3 -2,-1 -> 1,2
 		 */
+		 int[] windowCenter={50,50};
+		 //The integer Array windowCenter has two ELements widowCenter[0]= x Koordinate;
+		 // windowCenter[1]= y Koordinate of the Middle Point od the Window
+		// int x=50;//X koordinate of Middle Point set as 50
+		// int y=50;//Y Koordinate of Middle Point set as 50
+	//int[] WCkoord;// Array that saves the 2 Elements, the x Koordinate od the new Middle point, from 
+		// a neighbour Field , also the y Koordinate from this Field.
+		for(int i=-3;i<=3;i++){// tree neighbours of the Middle Field that are right from it , and left from it
+			if(i!=0){// i shouldnt be 0
+				//X koordinate is the same
+				int x=windowCenter[0];
+				int y=windowCenter[1]+i*sin60*radius;//or y=y+i*sin60*radius
+				//example: for i=1
+				// it gets the first right neighbours koordinates 
+				int []WCkoord={x,y};
+				//Save the new koordinate in fieldcoordinates TODO
+			}
+		}
+		for(int j=-2;j<=2;j++){//j is the amount of radiuses that we need in order to go one row higher/lower or three rows higher/lower
+			
+		if(j!=0){//j shouldnt be 0 
+			int x=windowCenter[0]+sin60*radius;
+			int y=windowCenter[1]+j*radius;
+			//if j=1, we get the koordinates of a field one row above our middle Field
+			int []WCkoord={x,y};
+			//Save those koordinates to fieldcoordinates TODO
+			//so now we have the koordinates of the field one row above the middle Field
+			// we go tree times right and left in order to get their neighbours coordinates
+			for(int i=-3;i<=3;i++){
+				if(i!=0);{
+				int v= WCkoord[0];//the neighbour has the same x koordinate
+				int u=WCkoord[1]+i*sin60*radius;
+				int[] NeighbourKoord1_3={v,u};//koordinates of the neighbours from the Field that we got first, starting from the Middle Field and going one row or three roads above or under 
+				//Problem: we have calculated more neighbours than we actually had, we calculated also the koordinates of Fiels that are with Water
+				//Filter them?
+				//Save into fieldCoordinates TODO
+			}
+			}
+		}
+		}
+		//In order to calculate the Coordinates of a Field, that is two rows above or under our Middle Field
+		for(int k=-2;k<=2;k++){
+			if(k!=-1){
+				if(k!=0){
+					if(k!=1){
+						int x=windowCenter[0];
+					   int y=windowCenter[1]+k*sin60*radius;
+					   int []WCkoord={x,y};
+					   //SAVE ,not done
+					   for(int i=-3;i<=3;i++){
+							if(i!=0);{
+							int g= WCkoord[0];//the neighbour has the same x koordinate
+							int h=WCkoord[1]+i*sin60*radius;
+							//Problem: we have calculated more neighbours than we actually had, we calculated also the koordinates od the Fiels that are with Water
+							//Filter them?
+							int [] NeighbourKoord2={g,h};
+							//Save into fieldCoordinates, not done
+						}
+					}
+					}
+				}
+			}
+		}
+		//WCkoord sind die Koordinaten der Felder der mitlleren Reihe von links nach rechts
+		//WCkoord dazu kommen die Koordinaten eines Feldes das eine Reihen unten ist
+		//NeighbourKoord1_3 Nachbarn diese Feldes von links nach recht
+		//WCkoord dazu  kommen die Koordinaten eines Feldes das eine Reihen oben ist
+		//NeighbourKoord1_3 Nachbarn diese Feldes von links nach recht
+		//WCKoord dazu kommen die Koordinaten eines Feldes das zwei Reihen oben ist
+		//NeighbourKoord1_3 Nachbarn diese Feldes von links nach recht
+		//WCkoord dazu kommen die Koordinaten des Feldes das zwei Reihen unten ist
+		//NeighbourKoord2 nachbarn dieses Feldes von links nach rechts
+		//WCkoord dazu kommen die Koordinaten des Feldes das zwei Reihen oben ist
+	    //NeighbourKoord2 Nachbarn dieses Feldes von links nach rechts
 
 	}
 
@@ -185,5 +259,6 @@ public class View implements ViewInterface {
 		// TODO Auto-generated method stub
 		return 0;
 	}
+	
 
 }
