912bf8f550e5055e8b059af4faefad0bce95a2c6
adam.mahmoud <adam.mahmoud@d5b95618-209e-438a-8481-01c1a498ce88>
2016-05-25 18:05:59 +0000

    Moved GameLogicInterface, added resources folder for json output

--- src/model/GameLogic.java
+++ src/model/GameLogic.java
@@ -2,7 +2,6 @@ package model;
 
 import java.util.ArrayList;
 
-import client.controller.GameLogicInterface;
 import enums.ResourceType;
 
 /**
--- src/model/GameLogicInterface.java
+++ src/model/GameLogicInterface.java
@@ -1,4 +1,4 @@
-package client.controller;
+package model;
 
 
 public interface GameLogicInterface {
