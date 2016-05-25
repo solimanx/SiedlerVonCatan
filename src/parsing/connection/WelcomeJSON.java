2f3b9808d7b02b45db39373150e26f69f116ad5a
adam.mahmoud <adam.mahmoud@d5b95618-209e-438a-8481-01c1a498ce88>
2016-05-25 04:19:16 +0000

    Added missing class for JSON

---
+++ src/parsing/connection/WelcomeJSON.java
@@ -0,0 +1,14 @@
+package parsing.connection;
+
+/**
+ * 
+ * <b>3: "Verbindungsaufbau"</b>
+ * <p>
+ * Check if server is compatible with the client via protocol and version.
+ * </p>
+ * 
+ * @version 0.1
+ */
+public class WelcomeJSON {
+
+}
