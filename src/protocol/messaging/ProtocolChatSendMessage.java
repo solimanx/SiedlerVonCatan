69c39ba96e343dbc0e32c996fb32ab441fd5bb54
amina.dacikj <amina.dacikj@d5b95618-209e-438a-8481-01c1a498ce88>
2016-05-27 17:30:55 +0000

    edit ProtocolChatSendMessage

--- src/protocol/messaging/ProtocolChatSendMessage.java
+++ src/protocol/messaging/ProtocolChatSendMessage.java
@@ -1,9 +1,17 @@
 package protocol.messaging;
+
 import com.google.gson.annotations.SerializedName;
 
 public class ProtocolChatSendMessage {
-	 @SerializedName("Chatnachricht senden")
-		public String sendMessage;
-		@SerializedName("Nachricht")
-		public String message ;
+
+	@SerializedName("Chatnachricht senden")
+	private String sendMessage;
+	@SerializedName("Nachricht")
+	private String message;
+
+	public ProtocolChatSendMessage(String sendMessage, String message) {
+
+		this.sendMessage = sendMessage;
+		this.message = message;
+	}
 }
