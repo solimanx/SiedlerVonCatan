57ff9f57aecbe1e4dcba98cbb2e9b3944ecae29f
amina.dacikj <amina.dacikj@d5b95618-209e-438a-8481-01c1a498ce88>
2016-05-27 12:15:27 +0000

    created ProtocolBuild

--- src/protocol/serverinstructions/ProtocolBuild.java
+++ src/protocol/serverinstructions/ProtocolBuild.java
@@ -1,5 +1,10 @@
 package protocol.serverinstructions;
 
+import com.google.gson.annotations.SerializedName;
+
 public class ProtocolBuild {
+
+	@SerializedName("Gebäude")
+	public ProtocolBuilding building;
 
 }
