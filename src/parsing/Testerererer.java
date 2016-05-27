d02cdb2b57ef0fb48073d4e287fa9a316784174b
k.staudacher <k.staudacher@d5b95618-209e-438a-8481-01c1a498ce88>
2016-05-27 10:52:25 +0000

    Testklasse für Json Objekte

---
+++ src/parsing/Testerererer.java
@@ -0,0 +1,28 @@
+package parsing;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import protocol.connection.ProtocolHello;
+
+public class Testerererer{
+	    public static void main(String[] args){
+
+	        String jsonstring = "{ \"Hallo\" : { \"Version\" : \"JavaFXClient 0.1 (RuhendeRebellionen)\" } }";
+	        System.out.println(jsonstring);
+	        Parser p = new Parser();
+	        Object o = p.parseString(jsonstring);
+	        System.out.println(o.getClass());
+	        ProtocolHello ph = (ProtocolHello) o;
+	        System.out.println("");
+	        System.out.println(ph.protokoll);
+	        System.out.println(ph.version);
+
+	        
+	        
+	       Gson gson = new GsonBuilder().serializeNulls().setPrettyPrinting().create();
+	       gson.toJson(o,System.out);
+	        
+
+	    }
+	}
