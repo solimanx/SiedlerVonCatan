62f747bf797545b98379c486d7b753f1e35af610
roxana.shaikh <roxana.shaikh@d5b95618-209e-438a-8481-01c1a498ce88>
2016-05-27 20:31:46 +0000

    protocolbuildrequest gets/constructor

--- src/protocol/clientinstructions/ProtocolBuildRequest.java
+++ src/protocol/clientinstructions/ProtocolBuildRequest.java
@@ -1,5 +1,33 @@
 package protocol.clientinstructions;
 
+import com.google.gson.annotations.SerializedName;
+
 public class ProtocolBuildRequest {
+
+	@SerializedName("Bauen")
+	private String build;
+	@SerializedName("Typ")
+	private String type;
+	@SerializedName("Ort")
+	private String location;
+
+	public ProtocolBuildRequest(String sendMessage, String message) {
+
+		this.build = build;
+		this.type = type;
+		this.location = location;
+	}
+
+	public String getBuild() {
+		return build;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public String getLocation() {
+		return location;
+	}
 
 }
