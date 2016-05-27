3fdb6df86624d82643d8fb991e81efdf75c6fce2
amina.dacikj <amina.dacikj@d5b95618-209e-438a-8481-01c1a498ce88>
2016-05-27 12:20:19 +0000

    created ProtocolResourceObtain

--- src/protocol/serverinstructions/ProtocolResourceObtain.java
+++ src/protocol/serverinstructions/ProtocolResourceObtain.java
@@ -1,5 +1,12 @@
 package protocol.serverinstructions;
 
+import com.google.gson.annotations.SerializedName;
+
 public class ProtocolResourceObtain {
+
+	@SerializedName("Spieler")
+	public int player;
+	@SerializedName("Rohstoffe")
+	public ProtocolResource resource;
 
 }
