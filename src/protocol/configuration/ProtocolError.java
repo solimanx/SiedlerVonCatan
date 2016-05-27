5531c2c089506d12fbcd5704b5ebb5585b666598
roxana.shaikh <roxana.shaikh@d5b95618-209e-438a-8481-01c1a498ce88>
2016-05-27 21:03:03 +0000

    ProtocolError added

--- src/protocol/configuration/ProtocolError.java
+++ src/protocol/configuration/ProtocolError.java
@@ -1,5 +1,20 @@
 package protocol.configuration;
 
+import com.google.gson.annotations.SerializedName;
+
 public class ProtocolError {
+
+	@SerializedName("Meldung")
+	private String notice;
+
+	public ProtocolError(String notice) {
+
+		this.notice = notice;
+
+	}
+
+	public String getNotice() {
+		return notice;
+	}
 
 }
