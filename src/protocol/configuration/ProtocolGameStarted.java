e27fef01d61d1f68b9dd552964e1ca841acec976
roxana.shaikh <roxana.shaikh@d5b95618-209e-438a-8481-01c1a498ce88>
2016-05-27 21:37:55 +0000

    GameStartedProtocol constructor/gets added

--- src/protocol/configuration/ProtocolGameStarted.java
+++ src/protocol/configuration/ProtocolGameStarted.java
@@ -1,7 +1,22 @@
 package protocol.configuration;
 
+import com.google.gson.annotations.SerializedName;
+
+import protocol.object.ProtocolResource;
+
 public class ProtocolGameStarted {
-	
-	//leere Nachricht
+
+	@SerializedName("Karte")
+	private ProtocolResource resources;
+
+	public ProtocolGameStarted() {
+
+		this.resources = resources;
+
+	}
+
+	public ProtocolResource getResources() {
+		return resources;
+	}
 
 }
