ac98baee2a8e09e97d8fda07e3a50750f84524d2
roxana.shaikh <roxana.shaikh@d5b95618-209e-438a-8481-01c1a498ce88>
2016-05-27 20:57:11 +0000

    gets/ constructor added to PlayerProfileProtocol

--- src/protocol/configuration/ProtocolPlayerProfile.java
+++ src/protocol/configuration/ProtocolPlayerProfile.java
@@ -1,5 +1,29 @@
 package protocol.configuration;
 
+import com.google.gson.annotations.SerializedName;
+
+import enums.protocol.ColorType;
+
 public class ProtocolPlayerProfile {
+
+	@SerializedName("Name")
+	private String name;
+	@SerializedName("Farbe")
+	private ColorType color;
+
+	public ProtocolPlayerProfile(String name, ColorType color) {
+
+		this.name = name;
+		this.color = color;
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public ColorType getColor() {
+		return color;
+	}
 
 }
