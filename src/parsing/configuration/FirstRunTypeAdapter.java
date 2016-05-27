073ad57789ff0cbf2c3b57ce900c1dd3ffdda7d5
matthias.moosburger <matthias.moosburger@d5b95618-209e-438a-8481-01c1a498ce88>
2016-05-27 08:10:58 +0000

    TypeAdapter #1

---
+++ src/parsing/configuration/FirstRunTypeAdapter.java
@@ -0,0 +1,33 @@
+package parsing.configuration;
+
+import java.io.IOException;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+/**
+ *
+ * <b>6: "Konfiguration und Spielstart"</b>
+ * <p>
+ * TODO write a better explanation
+ * </p>
+ *
+ * @version 0.1
+ */
+public class FirstRunTypeAdapter extends TypeAdapter{
+
+	@Override
+	public void write(JsonWriter out, Object value) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Object read(JsonReader in) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
--- src/parsing/connection/WelcomeTypeAdapter.java
+++ src/parsing/connection/WelcomeTypeAdapter.java
@@ -1,34 +1,36 @@
 package parsing.connection;
 
+import java.io.IOException;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
 import parsing.JSONInterface;
 
 /**
- * 
+ *
  * <b>3: "Verbindungsaufbau"</b>
  * <p>
  * Check if server is compatible with the client via protocol and version.
  * </p>
- * 
+ *
  * @version 0.1
  */
-public class WelcomeJSON implements JSONInterface<WelcomeJSON> {
+public class WelcomeTypeAdapter extends TypeAdapter {
 
 	@Override
-	public void createJSON(WelcomeJSON t) {
+	public void write(JsonWriter out, Object value) throws IOException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public WelcomeJSON getFromJSON(String json) {
+	public Object read(JsonReader in) throws IOException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
-	public void handleJSON() {
-		// TODO Auto-generated method stub
-		
-	}
+
 
 }
