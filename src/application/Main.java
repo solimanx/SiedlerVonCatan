a6c4047617fee30f3e8fc85bd6dca27e69dff024
matthias.moosburger <matthias.moosburger@d5b95618-209e-438a-8481-01c1a498ce88>
2016-05-30 18:59:56 +0000

    main start client

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import client.client.Client;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import server.controller.GameController;
@@ -13,10 +14,13 @@ public class Main extends Application {
 	private static Logger logger = LogManager.getLogger(Main.class.getName());
 	
 	private GameController gc;
+	private Client client;
 
 	@Override
 	public void start(Stage primaryStage) {
-		gc = new GameController(primaryStage,1); //one player Only
+		gc = new GameController(primaryStage,1);
+		client = new Client();
+		client.run();
 		
 	}
 	
