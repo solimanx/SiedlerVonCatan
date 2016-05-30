cfddd20c44f7c8086b3205f09f8077b75bfc2c73
matthias.moosburger <matthias.moosburger@d5b95618-209e-438a-8481-01c1a498ce88>
2016-05-30 18:53:59 +0000

    Client with connection tries

--- src/client/client/Client.java
+++ src/client/client/Client.java
@@ -8,23 +8,66 @@ import java.net.Socket;
 
 import com.google.gson.Gson;
 
-public class Client {
-	public static void main(String[] args) throws IOException {
-		Socket socket = new Socket("localhost", 8080); //TODO: alle paar Sekunden neu versuchen.
-		OutputStreamWriter writer = new OutputStreamWriter(socket.getOutputStream(), "UTF-8");
-		BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+public class Client extends Thread {
+	static int connectionTry = 0;
+	Socket socket = null;
+	OutputStreamWriter writer = null;
+	BufferedReader reader = null;
+	int port = 8080;
+	boolean scanning = true;
+	boolean connectionActive = false;
 
-		int i = 5;
-
-		writer.write(i + "\n");
-		writer.flush();
-
-
-		String line = reader.readLine();
-
-		System.out.println("Received from Server:\n" + line);
-
-		socket.close();
+	public void setPort(int port) {
+		this.port = port;
 	}
 
+	@Override
+	public void run() {
+		try {
+			socket = new Socket("localhost", port);
+			writer = new OutputStreamWriter(socket.getOutputStream(), "UTF-8");
+			reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+			scanning = false;
+			connectionActive = true;
+			runClient();
+		} catch (IOException e) {
+			System.out.println("Connection failed");
+			scanning = true;
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException ie) {
+				ie.printStackTrace();
+			}
+		} finally {
+			try {
+				socket.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void runClient() throws IOException {
+		while (connectionActive) {
+			String line = reader.readLine();
+			System.out.println("Received from Server:\n" + line);
+		}
+	}
+
+	private void write(String s) throws IOException {
+		writer.write(s + "\n");
+		writer.flush();
+	}
+
+	private void stopClient() {
+
+		try {
+			socket.close();
+			connectionActive = false;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
