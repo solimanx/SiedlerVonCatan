59042672c6df8c5cab38c71c337c82bea13342a1
amina.dacikj <amina.dacikj@d5b95618-209e-438a-8481-01c1a498ce88>
2016-06-01 08:24:26 +0000

    Logging Information Our first log

--- src/application/Main.java
+++ src/application/Main.java
@@ -28,6 +28,18 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
+		Logger logger = Logger.getLogger("MyLog");
+
+		FileHandler fileHandler = new FileHandler("Log.txt");
+		fileHandler = new FileHandler("MyLogFile.log", true);
+		
+		logger.addHandler(fileHandler);
+		logger.setLevel(Level.ALL);
+		
+		SimpleFormatter formatter = new SimpleFormatter();
+		fileHandler.setFormatter(formatter);
+		
+		logger.log(Level.INFO, "Our first log");
 		
 		if (mode) {
 			server = new Server();
