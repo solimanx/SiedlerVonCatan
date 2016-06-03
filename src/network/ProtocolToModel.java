ec58935c5457c945946c57633f3b3e0ed31b74a4
j.reff <j.reff@d5b95618-209e-438a-8481-01c1a498ce88>
2016-06-03 12:57:29 +0000

    fix

--- src/network/ProtocolToModel.java
+++ src/network/ProtocolToModel.java
@@ -4,7 +4,7 @@ import model.HexService;
 import protocol.object.ProtocolResource;
 
 public final class ProtocolToModel {
-	
+
 	public static int[] getFieldCoordinates(String field_id) {
 		return HexService.getFieldCoordinates(field_id);
 	}
@@ -12,28 +12,30 @@ public final class ProtocolToModel {
 	public static int getPlayerId(int thredId){
 		//TODO
 	}
-	
-	
-	
+
+
+
 	public static int[] getCornerCoordinates(String location){
 		//TODO
 		//return [x][y][dir]
-		
-		
+
+
 	}
-	
+
 	public static int[] getEdgeCoordinates(String location){
 		//TODO
 		//return [x][y][dir]
-		
-		
-		
+
+
+
 	}
-	
+
 	public static enums.PlayerState getPlayerState(String state){
 		//TODO
 	}
-	
+
+
+
 	public static int[] getResources(ProtocolResource resources){
 		//TODO
 	}
