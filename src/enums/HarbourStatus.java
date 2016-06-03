ca5a049708f804eed3818335e149d7ad491eaa37
adam.mahmoud <adam.mahmoud@d5b95618-209e-438a-8481-01c1a498ce88>
2016-06-03 06:11:40 +0000

    Removed serialization

--- src/enums/HarbourStatus.java
+++ src/enums/HarbourStatus.java
@@ -1,22 +1,5 @@
 package enums;
 
-import com.google.gson.annotations.SerializedName;
-
-/**
- * @see parsing.objects.HarbourTypeAdapter
- */
 public enum HarbourStatus {
-	@SerializedName("Hafen") THREE_TO_ONE,
-
-	@SerializedName("Holz Hafen") WOOD,
-
-	@SerializedName("Lehm Hafen") CLAY,
-
-	@SerializedName("Erz Hafen") ORE,
-
-	@SerializedName("Wolle Hafen") SHEEP,
-
-	@SerializedName("Getreide Hafen") CORN,
-
-	NULL // TODO: serialization
+	THREE_TO_ONE, WOOD, CLAY, ORE, SHEEP, CORN, NULL
 }
