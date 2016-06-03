8fe09009d070661d5f30913c8d81a5c7afb7fd5a
adam.mahmoud <adam.mahmoud@d5b95618-209e-438a-8481-01c1a498ce88>
2016-06-03 10:24:00 +0000

    Reverted to old Parser

--- src/parsing/Parser.java
+++ src/parsing/Parser.java
@@ -1,69 +1,93 @@
 package parsing;
 
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Map;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+
 public class Parser {
-	Response response;
-	Gson gson = new GsonBuilder().create();
-	HashMap<String,Object> map;
-
-	public Parser() {
-		map = new HashMap<String, Object>();
-	}
-
-	@SuppressWarnings("unchecked")
 	public <T> T parseString(String string) {
 
-		response = gson.fromJson(string, Response.class);
+		Gson gson = new GsonBuilder().create();
+		Response response = gson.fromJson(string, Response.class);
 
-		//TODO perhaps reference to variables so it doesn't have to refresh map everytime
-		refreshMap();
-	 
-		for ( HashMap.Entry<String, Object> entry : map.entrySet() ) {
-			if(entry.getValue()!=null){
-				return (T) entry.getValue();
-			}
-		   
+		if (response.pHello != null) {
+			return (T) response.pHello;
 		}
-		return null;
-	}
 
-	private void refreshMap() {
-		map.put("ProtocolHello", response.pHello);
-		map.put("ProtocolWelcome", response.pWelcome);
-		map.put("ProtocolClientReady", response.pClientReady);
-		map.put("ProtocolError", response.pError);
-		map.put("ProtocolGameStarted", response.pGameStarted);
-		map.put("ProtocolPlayerProfile", response.pPlayerProfile);
-		map.put("ProtocolVictory", response.pVictory);
-		map.put("ProtocolBuild", response.pBuild);
-		map.put("ProtocolDiceRollResult", response.pDRResult);
-		map.put("ProtocolResourceObtain", response.pRObtain);
-		map.put("Protocolstatusupdate", response.pSUpdate);
-		map.put("ProtocolCosts",response.pCosts);
-		map.put("ProtocolRobberMovement", response.pRobberMovement);
-		map.put("ProtocolChatReceiveMessage", response.pChatReceive);
-		map.put("ProtocolChatSendMessage", response.pChatSend);
-		map.put("ProtocolServerConfirmation", response.pServerConfirmation);
-		map.put("ProtocolBuildRequest", response.pBuildRequest);
-		map.put("ProtocolDiceRollRequest", response.pDiceRollRequest);
-		map.put("ProtocolEndTurn", response.pEndTurn);
-		map.put("ProtocolRobberLoss", response.pRobberLoss);
-		map.put("ProtocolRobberMovementRequest", response.pRobberMoveRequest);
-		map.put("ProtocolHarbourRequest", response.pHarbourRequest);
-		map.put("ProtocolTradeRequest", response.pTradeRequest);
-		map.put("ProtocolTradePreview",response.pTradePreview);
-		map.put("ProtocolTradeAccept", response.pTradeAccept);
-		map.put("ProtocolTradeConfirm", response.pTradeConfirm);
+		if (response.pWelcome != null) {
+			return (T) response.pWelcome;
+		}
+
+		//
+
+		if (response.pClientReady != null) {
+			return (T) response.pClientReady;
+		}
+
+		if (response.pError != null) {
+			return (T) response.pError;
+		}
+
+		if (response.pGameStarted != null) {
+			return (T) response.pGameStarted;
+		}
+
+		if (response.pPlayerProfile != null) {
+			return (T) response.pPlayerProfile;
+		}
+
+		//
+
+		if (response.pBuild != null) {
+			return (T) response.pBuild;
+		}
+
+		if (response.pDRResult != null) {
+			return (T) response.pDRResult;
+		}
+
+		if (response.pRObtain != null) {
+			return (T) response.pRObtain;
+		}
+
+		if (response.pSUpdate != null) {
+			return (T) response.pSUpdate;
+		}
+
+		// ...
+
+		if (response.pChatReceive != null) {
+			return (T) response.pChatReceive;
+		}
+
+		if (response.pChatSend != null) {
+			return (T) response.pChatSend;
+		}
+
+		if (response.pServerConfirmation != null) {
+			return (T) response.pServerConfirmation;
+		}
+
+		// ..
+
+		if (response.pBuildRequest != null) {
+			return (T) response.pBuildRequest;
+		}
+
+		if (response.pDiceRollRequest != null) {
+			return (T) response.pDiceRollRequest;
+		}
+
+		if (response.pEndTurn != null) {
+			return (T) response.pEndTurn;
+		}
+
+		return null;
 
 	}
 
 	public String createString(Object fromObject) {
+		Gson gson = new GsonBuilder().create();
 		String response = gson.toJson(fromObject, fromObject.getClass());
 		return response;
 	}
