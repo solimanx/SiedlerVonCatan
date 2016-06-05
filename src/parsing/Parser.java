c6fdb318a44e78f01cd6fd519a174a57cd1a1ea3
amina.dacikj <amina.dacikj@d5b95618-209e-438a-8481-01c1a498ce88>
2016-06-05 22:53:31 +0000

    Logging

--- src/parsing/Parser.java
+++ src/parsing/Parser.java
@@ -2,95 +2,99 @@ package parsing;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Parser {
-	@SuppressWarnings("unchecked")
-	public <T> T parseString(String string) {
+    @SuppressWarnings("unchecked")
+    private static Logger logger = LogManager.getLogger(Parser.class.getName());
 
-		Gson gson = new GsonBuilder().create();
-		Response response = gson.fromJson(string, Response.class);
+    public <T> T parseString(String string) {
 
-		if (response.pHello != null) {
-			return (T) response.pHello;
-		}
+        Gson gson = new GsonBuilder().create();
+        Response response = gson.fromJson(string, Response.class);
 
-		if (response.pWelcome != null) {
-			return (T) response.pWelcome;
-		}
+        if (response.pHello != null) {
+            return (T) response.pHello;
+        }
 
-		//
+        if (response.pWelcome != null) {
+            return (T) response.pWelcome;
+        }
 
-		if (response.pClientReady != null) {
-			return (T) response.pClientReady;
-		}
+        //
 
-		if (response.pError != null) {
-			return (T) response.pError;
-		}
+        if (response.pClientReady != null) {
+            return (T) response.pClientReady;
+        }
 
-		if (response.pGameStarted != null) {
-			return (T) response.pGameStarted;
-		}
+        if (response.pError != null) {
+            return (T) response.pError;
+        }
 
-		if (response.pPlayerProfile != null) {
-			return (T) response.pPlayerProfile;
-		}
+        if (response.pGameStarted != null) {
+            return (T) response.pGameStarted;
+        }
 
-		//
+        if (response.pPlayerProfile != null) {
+            return (T) response.pPlayerProfile;
+        }
 
-		if (response.pBuild != null) {
-			return (T) response.pBuild;
-		}
+        //
 
-		if (response.pDRResult != null) {
-			return (T) response.pDRResult;
-		}
+        if (response.pBuild != null) {
+            return (T) response.pBuild;
+        }
 
-		if (response.pRObtain != null) {
-			return (T) response.pRObtain;
-		}
+        if (response.pDRResult != null) {
+            return (T) response.pDRResult;
+        }
 
-		if (response.pSUpdate != null) {
-			return (T) response.pSUpdate;
-		}
+        if (response.pRObtain != null) {
+            return (T) response.pRObtain;
+        }
 
-		// ...
+        if (response.pSUpdate != null) {
+            return (T) response.pSUpdate;
+        }
 
-		if (response.pChatReceive != null) {
-			return (T) response.pChatReceive;
-		}
+        // ...
 
-		if (response.pChatSend != null) {
-			return (T) response.pChatSend;
-		}
+        if (response.pChatReceive != null) {
+            return (T) response.pChatReceive;
+        }
 
-		if (response.pServerConfirmation != null) {
-			return (T) response.pServerConfirmation;
-		}
+        if (response.pChatSend != null) {
+            return (T) response.pChatSend;
+        }
 
-		// ..
+        if (response.pServerConfirmation != null) {
+            return (T) response.pServerConfirmation;
+        }
 
-		if (response.pBuildRequest != null) {
-			return (T) response.pBuildRequest;
-		}
+        // ..
 
-		if (response.pDiceRollRequest != null) {
-			return (T) response.pDiceRollRequest;
-		}
+        if (response.pBuildRequest != null) {
+            return (T) response.pBuildRequest;
+        }
 
-		if (response.pEndTurn != null) {
-			return (T) response.pEndTurn;
-		}
+        if (response.pDiceRollRequest != null) {
+            return (T) response.pDiceRollRequest;
+        }
 
-		// TODO logging
-		System.out.println("CANNOT READ INPUT");
-		return null;
+        if (response.pEndTurn != null) {
+            return (T) response.pEndTurn;
+        }
 
-	}
+        logger.info("CANNOT READ INPUT");
+        System.out.println("CANNOT READ INPUT");
+        return null;
 
-	public String createString(Object fromObject) {
-		Gson gson = new GsonBuilder().create();
-		String response = gson.toJson(fromObject, fromObject.getClass());
-		return response;
-	}
+    }
+
+    public String createString(Object fromObject) {
+        Gson gson = new GsonBuilder().create();
+        String response = gson.toJson(fromObject, fromObject.getClass());
+        return response;
+    }
 }
