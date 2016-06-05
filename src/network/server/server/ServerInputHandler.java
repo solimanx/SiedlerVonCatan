4549d1b82f6228015a98c839404e361570cdeac0
amina.dacikj <amina.dacikj@d5b95618-209e-438a-8481-01c1a498ce88>
2016-06-05 20:01:30 +0000

    edit Method

--- src/network/server/server/ServerInputHandler.java
+++ src/network/server/server/ServerInputHandler.java
@@ -23,158 +23,159 @@ import protocol.serverinstructions.ProtocolResourceObtain;
 import protocol.serverinstructions.ProtocolStatusUpdate;
 
 public class ServerInputHandler extends InputHandler {
-	private ServerNetworkController serverNetworkController;
-	private int currentThreadID;
+    private ServerNetworkController serverNetworkController;
+    private int currentThreadID;
 
-	public ServerInputHandler(ServerNetworkController nc) {
-		super();
-		this.serverNetworkController = nc;
-	}
+    public ServerInputHandler(ServerNetworkController nc) {
+        super();
+        this.serverNetworkController = nc;
+    }
 
-	public ServerNetworkController getServerNetworkController() {
-		return serverNetworkController;
-	}
+    public ServerNetworkController getServerNetworkController() {
+        return serverNetworkController;
+    }
 
-	/**
-	 * sends JSON formatted string to parser and initiates handling of parsed
-	 * object
-	 *
-	 * @param s
-	 */
-	public void sendToParser(String s, int threadID) {
-		// speichert die threadID, falls sie in handle(Protocol...) gebraucht
-		// wird.
-		this.currentThreadID = threadID;
-		Object object = parser.parseString(s);
+    /**
+     * sends JSON formatted string to parser and initiates handling of parsed
+     * object
+     *
+     * @param s
+     */
+    public void sendToParser(String s, int threadID) {
+        // speichert die threadID, falls sie in handle(Protocol...) gebraucht
+        // wird.
+        this.currentThreadID = threadID;
+        Object object = parser.parseString(s);
 
-		handle(object);
-	}
+        handle(object);
+    }
 
-	@Override
-	protected void handle(ProtocolHello hello) {
-		System.out.println("SERVER: Hello gelesen!");
-		serverNetworkController.welcome(serverNetworkController.getPlayerModelId(currentThreadID));
+    @Override
+    protected void handle(ProtocolHello hello) {
+        System.out.println("SERVER: Hello gelesen!");
+        serverNetworkController.welcome(serverNetworkController.getPlayerModelId(currentThreadID));
 
-	}
+    }
 
-	@Override
-	protected void handle(ProtocolWelcome welcome) {
-		System.out.println("Welcome gelesen!");
-	}
+    @Override
+    protected void handle(ProtocolWelcome welcome) {
+        System.out.println("Welcome gelesen!");
+    }
 
-	// unnecessary Method in ServerInputHandler
-	@Override
-	protected void handle(ProtocolClientReady clientReady) {
-		serverNetworkController.clientReady(serverNetworkController.getPlayerModelId(currentThreadID));
+    // unnecessary Method in ServerInputHandler
+    @Override
+    protected void handle(ProtocolClientReady clientReady) {
+        serverNetworkController.clientReady(serverNetworkController.getPlayerModelId(currentThreadID));
 
-	}
+    }
 
-	@Override
-	protected void handle(ProtocolGameStarted gameStarted) {
-		// unnecessary Method in ServerInputHandler
+    @Override
+    protected void handle(ProtocolGameStarted gameStarted) {
+        // unnecessary Method in ServerInputHandler
 
-	}
+    }
 
-	@Override
-	protected void handle(ProtocolError error) {
-		// unnecessary Method in ServerInputHandler
+    @Override
+    protected void handle(ProtocolError error) {
+        // unnecessary Method in ServerInputHandler
 
-	}
+    }
 
-	@Override
-	protected void handle(ProtocolPlayerProfile playerProfile) {
-		String name = playerProfile.getName();
-		Color color = playerProfile.getColor();
-		// ClientNetworkController.sendPlayerProfile(name,color);
-		// TODO
+    @Override
+    protected void handle(ProtocolPlayerProfile playerProfile) {
+        String name = playerProfile.getName();
+        Color color = playerProfile.getColor();
+        ClientNetworkController clientNetworkController = new ClientNetworkController();
+        clientNetworkController.sendPlayerProfile(color, name);
 
-	}
 
-	@Override
-	protected void handle(ProtocolChatReceiveMessage chatReceiveMessage) {
-		String s = chatReceiveMessage.getMessage();
-		int playerId = chatReceiveMessage.getSender();
-		serverNetworkController.chatReceiveMessage(playerId, s);
-		/*
+    }
+
+    @Override
+    protected void handle(ProtocolChatReceiveMessage chatReceiveMessage) {
+        String s = chatReceiveMessage.getMessage();
+        int playerId = chatReceiveMessage.getSender();
+        serverNetworkController.chatReceiveMessage(playerId, s);
+        /*
 		 * ChatRecieveMessage, (Nachricht wird vom Server verteilt) needs to be
 		 * handled only in ServerOutputHandler and in ClientInputHandler.
 		 * unnecessary Method in ServerInputHandler
 		 */
-	}
+    }
 
-	protected void handle(ProtocolChatSendMessage chatSendMessage) {
-		String s = chatSendMessage.getMessage();
-		serverNetworkController.chatSendMessage(s, this.currentThreadID);
-	}
+    protected void handle(ProtocolChatSendMessage chatSendMessage) {
+        String s = chatSendMessage.getMessage();
+        serverNetworkController.chatSendMessage(s, this.currentThreadID);
+    }
 
-	@Override
-	protected void handle(ProtocolServerConfirmation serverConfirmation) {
-		String server_response = serverConfirmation.getServer_response();
-		serverNetworkController.serverConfirmation(server_response);
-		// Unnecessary Method in ServerInputHadler
-	}
+    @Override
+    protected void handle(ProtocolServerConfirmation serverConfirmation) {
+        String server_response = serverConfirmation.getServer_response();
+        serverNetworkController.serverConfirmation(server_response);
+        // Unnecessary Method in ServerInputHadler
+    }
 
-	//
-	@Override
-	protected void handle(ProtocolBuild build) {
-		// unnecessary Method in ServerInputHandler
+    //
+    @Override
+    protected void handle(ProtocolBuild build) {
+        // unnecessary Method in ServerInputHandler
 
-	}
+    }
 
-	@Override
-	protected void handle(ProtocolDiceRollResult diceRollResult) {
-		// unnecessary Method in ServerInputHandler
+    @Override
+    protected void handle(ProtocolDiceRollResult diceRollResult) {
+        // unnecessary Method in ServerInputHandler
 
-	}
+    }
 
-	@Override
-	protected void handle(ProtocolResourceObtain resourceObtain) {
-		// unnecessary Method in ServerInputHandler
-	}
+    @Override
+    protected void handle(ProtocolResourceObtain resourceObtain) {
+        // unnecessary Method in ServerInputHandler
+    }
 
-	@Override
-	protected void handle(ProtocolStatusUpdate statusUpdate) {
-		// unnecessary Method in ServerInputHandler
+    @Override
+    protected void handle(ProtocolStatusUpdate statusUpdate) {
+        // unnecessary Method in ServerInputHandler
 
-	}
+    }
 
-	//
+    //
 
-	@Override
-	protected void handle(ProtocolBuildRequest buildRequest) {
-		if (buildRequest.getBuilding() == "Straße") {
-			int[] loc = ProtocolToModel.getEdgeCoordinates(buildRequest.getLocation());
-			serverNetworkController.requestBuildStreet(loc[0], loc[1], loc[2],
-					serverNetworkController.getPlayerModelId(this.currentThreadID));
-		}
-		if (buildRequest.getBuilding() == "Dorf") {
-			int[] loc = ProtocolToModel.getCornerCoordinates(buildRequest.getLocation());
-			serverNetworkController.requestBuildVillage(loc[0], loc[1], loc[2],
-					serverNetworkController.getPlayerModelId(this.currentThreadID));
-		}
-		if (buildRequest.getBuilding() == "Stadt") {
-			int[] loc = ProtocolToModel.getCornerCoordinates(buildRequest.getLocation());
-			serverNetworkController.requestBuildCity(loc[0], loc[1], loc[2],
-					serverNetworkController.getPlayerModelId(this.currentThreadID));
-		}
-	}
+    @Override
+    protected void handle(ProtocolBuildRequest buildRequest) {
+        if (buildRequest.getBuilding() == "Straße") {
+            int[] loc = ProtocolToModel.getEdgeCoordinates(buildRequest.getLocation());
+            serverNetworkController.requestBuildStreet(loc[0], loc[1], loc[2],
+                    serverNetworkController.getPlayerModelId(this.currentThreadID));
+        }
+        if (buildRequest.getBuilding() == "Dorf") {
+            int[] loc = ProtocolToModel.getCornerCoordinates(buildRequest.getLocation());
+            serverNetworkController.requestBuildVillage(loc[0], loc[1], loc[2],
+                    serverNetworkController.getPlayerModelId(this.currentThreadID));
+        }
+        if (buildRequest.getBuilding() == "Stadt") {
+            int[] loc = ProtocolToModel.getCornerCoordinates(buildRequest.getLocation());
+            serverNetworkController.requestBuildCity(loc[0], loc[1], loc[2],
+                    serverNetworkController.getPlayerModelId(this.currentThreadID));
+        }
+    }
 
-	@Override
-	protected void handle(ProtocolDiceRollRequest diceRollRequest) {
-		serverNetworkController.diceRollRequest(serverNetworkController.getPlayerModelId(this.currentThreadID));
+    @Override
+    protected void handle(ProtocolDiceRollRequest diceRollRequest) {
+        serverNetworkController.diceRollRequest(serverNetworkController.getPlayerModelId(this.currentThreadID));
 
-	}
+    }
 
-	@Override
-	protected void handle(ProtocolEndTurn endTurn) {
-		System.out.println("Der Zug wurde beendet");
+    @Override
+    protected void handle(ProtocolEndTurn endTurn) {
+        System.out.println("Der Zug wurde beendet");
 
-	}
+    }
 
-	@Override
-	protected void handle(String string) {
-		// TODO Auto-generated method stub
+    @Override
+    protected void handle(String string) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
 }
