fca89edd7de49b2d5fe2cb72ab14120aca552293
amina.dacikj <amina.dacikj@d5b95618-209e-438a-8481-01c1a498ce88>
2016-06-05 22:49:03 +0000

    Logging

--- src/network/InputHandler.java
+++ src/network/InputHandler.java
@@ -1,5 +1,7 @@
 package network;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import parsing.Parser;
 import protocol.clientinstructions.ProtocolBuildRequest;
 import protocol.clientinstructions.ProtocolDiceRollRequest;
@@ -20,118 +22,120 @@ import protocol.serverinstructions.ProtocolRobberMovement;
 import protocol.serverinstructions.ProtocolStatusUpdate;
 
 public abstract class InputHandler {
-	protected Parser parser;
+    private static Logger logger = LogManager.getLogger(InputHandler.class.getName());
+    protected Parser parser;
 
-	public InputHandler() {
-		parser = new Parser();
-	}
+    public InputHandler() {
+        parser = new Parser();
+    }
 
-	// public void sendToParser();
+    // public void sendToParser();
 
-	public Parser getParser() {
-		return parser;
-	}
+    public Parser getParser() {
+        return parser;
+    }
 
-	/**
-	 * takes parsed object and redirects to its proper method depending on
-	 * objects "real" class. ( object will be casted to "real" class )
-	 *
-	 * @param o
-	 */
-	protected void handle(Object o) {
-		switch (o.getClass().getSimpleName()) {
-		case "ProtocolHello":
-			handle((ProtocolHello) o);
-			break;
-		case "ProtocolWelcome":
-			handle((ProtocolWelcome) o);
-			break;
-		case "ProtocolClientReady":
-			handle((ProtocolClientReady) o);
-			break;
-		case "ProtocolError":
-			handle((ProtocolError) o);
-			break;
-		case "ProtocolGameStarted":
-			handle((ProtocolGameStarted) o);
-			break;
-		case "ProtocolPlayerProfile":
-			handle((ProtocolPlayerProfile) o);
-			break;
-		case "ProtocolChatReceiveMessage":
-			handle((ProtocolChatReceiveMessage) o);
-			break;
-		case "ProtocolChatSendMessage":
-			handle((ProtocolChatSendMessage) o);
-			break;
-		case "ProtocolServerConfirmation":
-			handle((ProtocolServerConfirmation) o);
-			break;
-		case "ProtocolBuild":
-			handle((ProtocolBuild) o);
-			break;
-		case "ProtocolDiceRollResult":
-			handle((ProtocolDiceRollResult) o);
-			break;
-		case "ProtocolResourceObtain":
-			handle((ProtocolResourceObtain) o);
-			break;
-		case "ProtocolStatusUpdate":
-			handle((ProtocolStatusUpdate) o);
-			break;
-		case "ProtocolBuildRequest":
-			handle((ProtocolBuildRequest) o);
-			break;
-		case "ProtocolDiceRollRequest":
-			handle((ProtocolDiceRollRequest) o);
-			break;
-		case "ProtocolEndTurn":
-			handle((ProtocolEndTurn) o);
-			break;
-		case "ProtocolRobberMovement": // 0.2
-			handle((ProtocolRobberMovement) o);
-			break;
-		case "String":
-			handle((String) o);
-			break;
-		default:
-			System.out.println("Class not found"); // TODO logging
-		}
+    /**
+     * takes parsed object and redirects to its proper method depending on
+     * objects "real" class. ( object will be casted to "real" class )
+     *
+     * @param o
+     */
+    protected void handle(Object o) {
+        switch (o.getClass().getSimpleName()) {
+            case "ProtocolHello":
+                handle((ProtocolHello) o);
+                break;
+            case "ProtocolWelcome":
+                handle((ProtocolWelcome) o);
+                break;
+            case "ProtocolClientReady":
+                handle((ProtocolClientReady) o);
+                break;
+            case "ProtocolError":
+                handle((ProtocolError) o);
+                break;
+            case "ProtocolGameStarted":
+                handle((ProtocolGameStarted) o);
+                break;
+            case "ProtocolPlayerProfile":
+                handle((ProtocolPlayerProfile) o);
+                break;
+            case "ProtocolChatReceiveMessage":
+                handle((ProtocolChatReceiveMessage) o);
+                break;
+            case "ProtocolChatSendMessage":
+                handle((ProtocolChatSendMessage) o);
+                break;
+            case "ProtocolServerConfirmation":
+                handle((ProtocolServerConfirmation) o);
+                break;
+            case "ProtocolBuild":
+                handle((ProtocolBuild) o);
+                break;
+            case "ProtocolDiceRollResult":
+                handle((ProtocolDiceRollResult) o);
+                break;
+            case "ProtocolResourceObtain":
+                handle((ProtocolResourceObtain) o);
+                break;
+            case "ProtocolStatusUpdate":
+                handle((ProtocolStatusUpdate) o);
+                break;
+            case "ProtocolBuildRequest":
+                handle((ProtocolBuildRequest) o);
+                break;
+            case "ProtocolDiceRollRequest":
+                handle((ProtocolDiceRollRequest) o);
+                break;
+            case "ProtocolEndTurn":
+                handle((ProtocolEndTurn) o);
+                break;
+            case "ProtocolRobberMovement": // 0.2
+                handle((ProtocolRobberMovement) o);
+                break;
+            case "String":
+                handle((String) o);
+                break;
+            default:
+                System.out.println("Class not found");
+                logger.info("Class not found");
+        }
 
-	}
+    }
 
-	protected abstract void handle(ProtocolHello hello);
+    protected abstract void handle(ProtocolHello hello);
 
-	protected abstract void handle(ProtocolWelcome welcome);
+    protected abstract void handle(ProtocolWelcome welcome);
 
-	protected abstract void handle(ProtocolClientReady clientReady);
+    protected abstract void handle(ProtocolClientReady clientReady);
 
-	protected abstract void handle(ProtocolGameStarted gameStarted);
+    protected abstract void handle(ProtocolGameStarted gameStarted);
 
-	protected abstract void handle(ProtocolError error);
+    protected abstract void handle(ProtocolError error);
 
-	protected abstract void handle(ProtocolPlayerProfile playerProfile);
+    protected abstract void handle(ProtocolPlayerProfile playerProfile);
 
-	protected abstract void handle(ProtocolChatReceiveMessage chatReceiveMessage);
+    protected abstract void handle(ProtocolChatReceiveMessage chatReceiveMessage);
 
-	protected abstract void handle(ProtocolChatSendMessage chatSendMessage);
+    protected abstract void handle(ProtocolChatSendMessage chatSendMessage);
 
-	protected abstract void handle(ProtocolServerConfirmation serverConfirmation);
+    protected abstract void handle(ProtocolServerConfirmation serverConfirmation);
 
-	protected abstract void handle(String string);
+    protected abstract void handle(String string);
 
-	protected abstract void handle(ProtocolBuild build);
+    protected abstract void handle(ProtocolBuild build);
 
-	protected abstract void handle(ProtocolDiceRollResult diceRollResult);
+    protected abstract void handle(ProtocolDiceRollResult diceRollResult);
 
-	protected abstract void handle(ProtocolResourceObtain resourceObtain);
+    protected abstract void handle(ProtocolResourceObtain resourceObtain);
 
-	protected abstract void handle(ProtocolStatusUpdate statusUpdate);
+    protected abstract void handle(ProtocolStatusUpdate statusUpdate);
 
-	protected abstract void handle(ProtocolBuildRequest buildRequest);
+    protected abstract void handle(ProtocolBuildRequest buildRequest);
 
-	protected abstract void handle(ProtocolDiceRollRequest diceRollRequest);
+    protected abstract void handle(ProtocolDiceRollRequest diceRollRequest);
 
-	protected abstract void handle(ProtocolEndTurn endTurn);
+    protected abstract void handle(ProtocolEndTurn endTurn);
 
 }
