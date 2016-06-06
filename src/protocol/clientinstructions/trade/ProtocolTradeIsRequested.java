ed73b044daa81e24e3109f8054445f03c33dd6f2
amina.dacikj <amina.dacikj@d5b95618-209e-438a-8481-01c1a498ce88>
2016-06-06 02:48:49 +0000

    created ProtocolTradeIsRequested

---
+++ src/protocol/clientinstructions/trade/ProtocolTradeIsRequested.java
@@ -0,0 +1,43 @@
+package protocol.clientinstructions.trade;
+
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.annotations.Since;
+import protocol.object.ProtocolResource;
+
+@Since(0.2)
+public class ProtocolTradeIsRequested {
+
+    @SerializedName("Spieler")
+    private int player_id;
+    @SerializedName("Handel id")
+    private int trade_id;
+
+    @SerializedName("Angebot")
+    private ProtocolResource offer;
+
+    @SerializedName("Nachfrage")
+    private ProtocolResource withdrawal;
+
+    public ProtocolTradeIsRequested(int player_id, int trade_id, ProtocolResource offer, ProtocolResource withdrawal) {
+        this.player_id = player_id;
+        this.trade_id = trade_id;
+        this.offer = offer;
+        this.withdrawal = withdrawal;
+    }
+
+    public int getPlayer_id() {
+        return player_id;
+    }
+
+    public int getTrade_id() {
+        return trade_id;
+    }
+
+    public ProtocolResource getOffer() {
+        return offer;
+    }
+
+    public ProtocolResource getWithdrawal() {
+        return withdrawal;
+    }
+}
