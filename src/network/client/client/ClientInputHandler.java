cf0117edcf41e38532b831af1152a640b042d3b0
amina.dacikj <amina.dacikj@d5b95618-209e-438a-8481-01c1a498ce88>
2016-06-06 05:49:07 +0000

    created and implemented handle Methods

--- src/network/client/client/ClientInputHandler.java
+++ src/network/client/client/ClientInputHandler.java
@@ -20,156 +20,166 @@ import protocol.messaging.ProtocolChatSendMessage;
 import protocol.messaging.ProtocolServerConfirmation;
 import protocol.object.ProtocolBoard;
 import protocol.object.ProtocolBuilding;
+import protocol.object.ProtocolResource;
 import protocol.serverinstructions.ProtocolBuild;
 import protocol.serverinstructions.ProtocolDiceRollResult;
 import protocol.serverinstructions.ProtocolResourceObtain;
 import protocol.serverinstructions.ProtocolStatusUpdate;
+import protocol.serverinstructions.trade.ProtocolTradeIsRequested;
 
 public class ClientInputHandler extends InputHandler {
-	private ClientNetworkController networkController;
+    private ClientNetworkController networkController;
 
-	public ClientInputHandler(ClientNetworkController nc) {
-		super();
-		this.networkController = nc;
-	}
+    public ClientInputHandler(ClientNetworkController nc) {
+        super();
+        this.networkController = nc;
+    }
 
-	/**
-	 * sends JSON formatted string to parser and initiates handling of parsed
-	 * object
-	 *
-	 * @param s
-	 */
-	public void sendToParser(String s) {
-		Object object = parser.parseString(s);
-		handle(object);
-	}
+    /**
+     * sends JSON formatted string to parser and initiates handling of parsed
+     * object
+     *
+     * @param s
+     */
+    public void sendToParser(String s) {
+        Object object = parser.parseString(s);
+        handle(object);
+    }
 
-	@Override
-	protected void handle(ProtocolHello hello) {
-		System.out.println("Hello gelesen!");
-		networkController.serverHello(hello.getVersion(), hello.getProtocol());
-	}
+    @Override
+    protected void handle(ProtocolHello hello) {
+        System.out.println("Hello gelesen!");
+        networkController.serverHello(hello.getVersion(), hello.getProtocol());
+    }
 
-	@Override
-	protected void handle(ProtocolWelcome welcome) {
-		System.out.println("Willkommen gelesen!");
-		networkController.welcome(welcome.getPlayer_id());
-	}
+    @Override
+    protected void handle(ProtocolWelcome welcome) {
+        System.out.println("Willkommen gelesen!");
+        networkController.welcome(welcome.getPlayer_id());
+    }
 
-	@Override
-	protected void handle(ProtocolClientReady clientReady) {
-		// unnecessary Method in ClientInputHandler
+    @Override
+    protected void handle(ProtocolClientReady clientReady) {
+        // unnecessary Method in ClientInputHandler
 
-	}
+    }
 
-	@Override
-	protected void handle(ProtocolGameStarted gameStarted) {
-		ProtocolBoard board=gameStarted.getBoard();
-		//networkController.gameStarted(board);
+    @Override
+    protected void handle(ProtocolGameStarted gameStarted) {
+        ProtocolBoard board = gameStarted.getBoard();
+        //networkController.gameStarted(board);
 
-	}
+    }
 
-	@Override
-	protected void handle(ProtocolError error) {
-		System.out.println("Meldung wird geschickt");
-		networkController.error(error.getNotice());
+    @Override
+    protected void handle(ProtocolError error) {
+        System.out.println("Meldung wird geschickt");
+        networkController.error(error.getNotice());
 
-	}
+    }
 
-	@Override
-	protected void handle(ProtocolPlayerProfile playerProfile) {
-		// unnecessary Method in ClientInputHandler
+    @Override
+    protected void handle(ProtocolPlayerProfile playerProfile) {
+        // unnecessary Method in ClientInputHandler
 
-	}
+    }
 
-	@Override
-	protected void handle(ProtocolChatReceiveMessage chatReceiveMessage) {
-		String s = chatReceiveMessage.getMessage();
-		int playerId = chatReceiveMessage.getSender();
-		networkController.chatReceiveMessage(playerId, s);
-	}
+    @Override
+    protected void handle(ProtocolChatReceiveMessage chatReceiveMessage) {
+        String s = chatReceiveMessage.getMessage();
+        int playerId = chatReceiveMessage.getSender();
+        networkController.chatReceiveMessage(playerId, s);
+    }
 
-	@Override
-	protected void handle(ProtocolChatSendMessage chatSendMessage) {
-		// unnecessary Method in ClientInputHandler
+    @Override
+    protected void handle(ProtocolChatSendMessage chatSendMessage) {
+        // unnecessary Method in ClientInputHandler
 
-	}
+    }
 
-	@Override
-	protected void handle(ProtocolServerConfirmation serverConfirmation) {
-		String server_response = serverConfirmation.getServer_response();
-		networkController.serverConfirmation(server_response);
-	}
+    @Override
+    protected void handle(ProtocolServerConfirmation serverConfirmation) {
+        String server_response = serverConfirmation.getServer_response();
+        networkController.serverConfirmation(server_response);
+    }
 
-	//
-	@Override
-	protected void handle(ProtocolBuild build) {
-		ProtocolBuilding building = build.getBuilding();
-		// networkController.build(building);
+    //
+    @Override
+    protected void handle(ProtocolBuild build) {
+        ProtocolBuilding building = build.getBuilding();
+        // networkController.build(building);
 
-	}
+    }
 
-	@Override
-	protected void handle(ProtocolDiceRollResult diceRollResult) {
-		int playerID = diceRollResult.getPlayer();
-		int result = diceRollResult.getRoll();
-		networkController.diceRollResult(playerID, result);
-	}
+    @Override
+    protected void handle(ProtocolDiceRollResult diceRollResult) {
+        int playerID = diceRollResult.getPlayer();
+        int result = diceRollResult.getRoll();
+        networkController.diceRollResult(playerID, result);
+    }
 
-	@Override
-	protected void handle(ProtocolResourceObtain resourceObtain) {
-		// int playerID = ProtocolResourceObtain.getPlayer();
-		// int[]
-		// resources=ProtocolToModel.getResources(resourceObtain.getPlayer().getResources());
-		// networkController.resourceObtain(playerID, resources);
+    @Override
+    protected void handle(ProtocolResourceObtain resourceObtain) {
+        // int playerID = ProtocolResourceObtain.getPlayer();
+        // int[]
+        // resources=ProtocolToModel.getResources(resourceObtain.getPlayer().getResources());
+        // networkController.resourceObtain(playerID, resources);
 
-	}
+    }
 
-	@Override
-	protected void handle(ProtocolStatusUpdate statusUpdate) {
-		// TODO Auto-generated method stub
-		// int playerId =
-		// ProtocolToModel.getPlayerId(statusUpdate.getPlayer().getPlayer_id());
-		enums.Color color = statusUpdate.getPlayer().getColor();
-		String name = statusUpdate.getPlayer().getName();
-		enums.PlayerState status = ProtocolToModel.getPlayerState(statusUpdate.getPlayer().getStatus());
-		int victoryPoints = statusUpdate.getPlayer().getVictory_points();
-		// int[] resources =
-		// ProtocolToModel.getResources(statusUpdate.getPlayer().getResources());
-		// networkController.statusUpdate(playerId, color, name, status,
-		// victoryPoints, resources);
+    @Override
+    protected void handle(ProtocolStatusUpdate statusUpdate) {
+        // TODO Auto-generated method stub
+        // int playerId =
+        // ProtocolToModel.getPlayerId(statusUpdate.getPlayer().getPlayer_id());
+        enums.Color color = statusUpdate.getPlayer().getColor();
+        String name = statusUpdate.getPlayer().getName();
+        enums.PlayerState status = ProtocolToModel.getPlayerState(statusUpdate.getPlayer().getStatus());
+        int victoryPoints = statusUpdate.getPlayer().getVictory_points();
+        // int[] resources =
+        // ProtocolToModel.getResources(statusUpdate.getPlayer().getResources());
+        // networkController.statusUpdate(playerId, color, name, status,
+        // victoryPoints, resources);
 
-	}
+    }
 
-	//
-	@Override
-	protected void handle(ProtocolBuildRequest buildRequest) {
-		// unnecessary Method in CLientInputHaendler
+    //
+    @Override
+    protected void handle(ProtocolBuildRequest buildRequest) {
+        // unnecessary Method in CLientInputHaendler
 
-	}
+    }
 
-	@Override
-	protected void handle(ProtocolDiceRollRequest diceRollRequest) {
-		// unnecessary Method in ClientInputHandler
+    @Override
+    protected void handle(ProtocolDiceRollRequest diceRollRequest) {
+        // unnecessary Method in ClientInputHandler
 
-	}
+    }
 
-	@Override
-	protected void handle(ProtocolEndTurn endTurn) {
-		System.out.println("Der Zug wurde beendet");
-		networkController.endTurn();
-		// unnecessary Method in ClientInputHandler
-	}
+    @Override
+    protected void handle(ProtocolEndTurn endTurn) {
+        System.out.println("Der Zug wurde beendet");
+        networkController.endTurn();
+        // unnecessary Method in ClientInputHandler
+    }
 
-	@Override
-	protected void handle(String string) {
-		networkController.serverConfirmation(string);
-	}
-	
-	protected void handle(ProtocolRobberLoss losses) {
-		//TODO 
-		
-	}
+    @Override
+    protected void handle(String string) {
+        networkController.serverConfirmation(string);
+    }
+
+    protected void handle(ProtocolRobberLoss losses) {
+        //TODO
+
+    }
+
+    protected void hnadle(ProtocolTradeIsRequested tradeIsRequested) {
+        int player_id = tradeIsRequested.getPlayer_id();
+        int trade_id = tradeIsRequested.getTrade_id();
+        ProtocolResource offer = tradeIsRequested.getOffer();
+        ProtocolResource withdrawal = tradeIsRequested.getWithdrawal();
+        //networkController.tradeIsRequested(player_id,trade_id,offer,withdrawal);
+    }
 
 
 }
