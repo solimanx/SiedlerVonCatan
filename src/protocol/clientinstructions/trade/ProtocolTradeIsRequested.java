3aeb3aac0f554f792f63ed95833c5e2cf852ac1c
amina.dacikj <amina.dacikj@d5b95618-209e-438a-8481-01c1a498ce88>
2016-06-06 02:59:28 +0000

    edit ProtocolTradeIsRequested

--- src/protocol/clientinstructions/trade/ProtocolTradeIsRequested.java
+++ src/protocol/clientinstructions/trade/ProtocolTradeIsRequested.java
@@ -4,11 +4,19 @@ import com.google.gson.annotations.SerializedName;
 import com.google.gson.annotations.Since;
 import protocol.object.ProtocolResource;
 
+/**
+ * <b>Handelsangebot(Request trade/offer is sent )</b>
+ * <p>
+ * Server sends request trade/offer to Players. They can decide to accept the request or reject it.
+ * </p>
+ */
+
 @Since(0.2)
 public class ProtocolTradeIsRequested {
 
     @SerializedName("Spieler")
     private int player_id;
+
     @SerializedName("Handel id")
     private int trade_id;
 
@@ -19,6 +27,7 @@ public class ProtocolTradeIsRequested {
     private ProtocolResource withdrawal;
 
     public ProtocolTradeIsRequested(int player_id, int trade_id, ProtocolResource offer, ProtocolResource withdrawal) {
+
         this.player_id = player_id;
         this.trade_id = trade_id;
         this.offer = offer;
@@ -26,18 +35,22 @@ public class ProtocolTradeIsRequested {
     }
 
     public int getPlayer_id() {
+
         return player_id;
     }
 
     public int getTrade_id() {
+
         return trade_id;
     }
 
     public ProtocolResource getOffer() {
+
         return offer;
     }
 
     public ProtocolResource getWithdrawal() {
+
         return withdrawal;
     }
 }
