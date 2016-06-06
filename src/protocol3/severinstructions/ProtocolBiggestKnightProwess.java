f77552649633a5e4c86be85c4297065f694b3bb9
amina.dacikj <amina.dacikj@d5b95618-209e-438a-8481-01c1a498ce88>
2016-06-06 16:56:52 +0000

    edit Protocol3

--- src/protocol3/severinstructions/ProtocolBiggestKnightProwess.java
+++ src/protocol3/severinstructions/ProtocolBiggestKnightProwess.java
@@ -1,7 +1,24 @@
 package protocol3.severinstructions;
 
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.annotations.Since;
+
 /**
  * Created  on 06.06.2016.
  */
+@Since(0.3)
 public class ProtocolBiggestKnightProwess {
+
+    @SerializedName("Spieler")
+
+    int player_id;
+
+    public ProtocolBiggestKnightProwess(int player_id) {
+        this.player_id = player_id;
+    }
+
+    public int getPlayer_id() {
+
+        return player_id;
+    }
 }
--- src/protocol3/severinstructions/ProtocolLongestRoad.java
+++ src/protocol3/severinstructions/ProtocolLongestRoad.java
@@ -1,8 +1,24 @@
 package protocol3.severinstructions;
 
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.annotations.Since;
+
 /**
  * Created on 06.06.2016.
  */
 
+@Since(0.3)
 public class ProtocolLongestRoad {
+
+    @SerializedName("Spieler")
+    int player_id;
+
+    public ProtocolLongestRoad(int player_id) {
+        this.player_id = player_id;
+    }
+
+    public int getPlayer_id() {
+
+        return player_id;
+    }
 }
