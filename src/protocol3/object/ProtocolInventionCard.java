ae03f0035bd7c8b273f94f721de151ccb56ebc73
roxana.shaikh <roxana.shaikh@d5b95618-209e-438a-8481-01c1a498ce88>
2016-06-07 18:32:54 +0000

    ProtocolInventionCard added

--- src/protocol3/object/ProtocolInventionCard.java
+++ src/protocol3/object/ProtocolInventionCard.java
@@ -1,7 +1,27 @@
 package protocol3.object;
 
-/**
- * Createdon 06.06.2016.
- */
+import com.google.gson.annotations.SerializedName;
+
+import protocol.object.ProtocolResource;
+
+
 public class ProtocolInventionCard {
+	@SerializedName("Rohstoffe")
+	ProtocolResource resource;
+
+	@SerializedName("Spieler")
+	int player_id;
+
+	public ProtocolInventionCard (ProtocolResource resource, int player_id) {
+		this.resource = resource;
+		this.player_id = player_id;
+	}
+
+	public ProtocolResource getResource() {
+		return resource;
+	}
+
+	public int getPlayer_id() {
+		return player_id;
+	}
 }
