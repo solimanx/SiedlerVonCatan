7f6c9d79a09f11a8b062c43f69121a08bcf66178
roxana.shaikh <roxana.shaikh@d5b95618-209e-438a-8481-01c1a498ce88>
2016-06-07 23:52:48 +0000

    ProtocolMonopolyCard

--- src/protocol3/object/ProtocolMonopolyCard.java
+++ src/protocol3/object/ProtocolMonopolyCard.java
@@ -1,7 +1,23 @@
 package protocol3.object;
 
+import com.google.gson.annotations.SerializedName;
+
+import protocol.object.ProtocolResource;
+
 /**
  * Created on 06.06.2016.
  */
 public class ProtocolMonopolyCard {
+	
+	@SerializedName("Rohstoffe")
+	ProtocolResource resource;
+	
+	public ProtocolMonopolyCard(ProtocolResource resource) {
+		this.resource = resource;
+		
+	}
+
+	public ProtocolResource getResource() {
+		return resource;
+
 }
