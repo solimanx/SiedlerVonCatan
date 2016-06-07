4f5a7393a617fbbc3ec6b486391350a458a37578
amina.dacikj <amina.dacikj@d5b95618-209e-438a-8481-01c1a498ce88>
2016-06-07 11:39:35 +0000

    added Protocol 0.2 and 0.3 to parser

--- src/parsing/Parser.java
+++ src/parsing/Parser.java
@@ -6,95 +6,136 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Parser {
-	@SuppressWarnings("unchecked")
-	private static Logger logger = LogManager.getLogger(Parser.class.getName());
+    @SuppressWarnings("unchecked")
+    private static Logger logger = LogManager.getLogger(Parser.class.getName());
 
-	public <T> T parseString(String string) {
+    public <T> T parseString(String string) {
 
-		Gson gson = new GsonBuilder().create();
-		Response response = gson.fromJson(string, Response.class);
+        Gson gson = new GsonBuilder().create();
+        Response response = gson.fromJson(string, Response.class);
 
-		if (response.pHello != null) {
-			return (T) response.pHello;
-		}
+        if (response.pHello != null) {
+            return (T) response.pHello;
+        }
 
-		if (response.pWelcome != null) {
-			return (T) response.pWelcome;
-		}
+        if (response.pWelcome != null) {
+            return (T) response.pWelcome;
+        }
 
-		//
+        //
 
-		if (response.pClientReady != null) {
-			return (T) response.pClientReady;
-		}
+        if (response.pClientReady != null) {
+            return (T) response.pClientReady;
+        }
 
-		if (response.pError != null) {
-			return (T) response.pError;
-		}
+        if (response.pError != null) {
+            return (T) response.pError;
+        }
 
-		if (response.pGameStarted != null) {
-			return (T) response.pGameStarted;
-		}
+        if (response.pGameStarted != null) {
+            return (T) response.pGameStarted;
+        }
 
-		if (response.pPlayerProfile != null) {
-			return (T) response.pPlayerProfile;
-		}
+        if (response.pPlayerProfile != null) {
+            return (T) response.pPlayerProfile;
+        }
 
-		//
+        //
 
-		if (response.pBuild != null) {
-			return (T) response.pBuild;
-		}
+        if (response.pBuild != null) {
+            return (T) response.pBuild;
+        }
 
-		if (response.pDRResult != null) {
-			return (T) response.pDRResult;
-		}
+        if (response.pDRResult != null) {
+            return (T) response.pDRResult;
+        }
 
-		if (response.pRObtain != null) {
-			return (T) response.pRObtain;
-		}
+        if (response.pRObtain != null) {
+            return (T) response.pRObtain;
+        }
 
-		if (response.pSUpdate != null) {
-			return (T) response.pSUpdate;
-		}
+        if (response.pSUpdate != null) {
+            return (T) response.pSUpdate;
+        }
 
-		// ...
+        // ...
 
-		if (response.pChatReceive != null) {
-			return (T) response.pChatReceive;
-		}
+        if (response.pChatReceive != null) {
+            return (T) response.pChatReceive;
+        }
 
-		if (response.pChatSend != null) {
-			return (T) response.pChatSend;
-		}
+        if (response.pChatSend != null) {
+            return (T) response.pChatSend;
+        }
 
-		if (response.pServerConfirmation != null) {
-			return (T) response.pServerConfirmation;
-		}
+        if (response.pServerConfirmation != null) {
+            return (T) response.pServerConfirmation;
+        }
 
-		// ..
+        // ..
 
-		if (response.pBuildRequest != null) {
-			return (T) response.pBuildRequest;
-		}
+        if (response.pBuildRequest != null) {
+            return (T) response.pBuildRequest;
+        }
 
-		if (response.pDiceRollRequest != null) {
-			return (T) response.pDiceRollRequest;
-		}
+        if (response.pDiceRollRequest != null) {
+            return (T) response.pDiceRollRequest;
+        }
 
-		if (response.pEndTurn != null) {
-			return (T) response.pEndTurn;
-		}
+        if (response.pEndTurn != null) {
+            return (T) response.pEndTurn;
+        }
+        //in Protocol 0.2
+        if (response.pVictory != null) {
+            return (T) response.pVictory;
+        }
 
-		logger.info("CANNOT READ INPUT");
-		System.out.println("CANNOT READ INPUT");
-		return null;
+        if (response.pCosts != null) {
+            return (T) response.pCosts;
+        }
 
-	}
+        if (response.pRobberMovement != null) {
+            return (T) response.pRobberMovement;
+        }
 
-	public String createString(Object fromObject) {
-		Gson gson = new GsonBuilder().create();
-		String response = gson.toJson(fromObject, fromObject.getClass());
-		return response;
-	}
+        if (response.pRobberLoss != null) {
+            return (T) response.pRobberLoss;
+
+        }
+
+        //in Protocol 0.3
+        if (response.pBiggestKnightProwess != null) {
+            return (T) response.pBiggestKnightProwess;
+        }
+
+        if (response.pInventionCard != null) {
+            return (T) response.pInventionCard;
+        }
+
+        if (response.pLongestRoad != null) {
+            return (T) response.pLongestRoad;
+        }
+        if (response.pMonopolyCardInfo != null) {
+            return (T) response.pMonopolyCardInfo;
+        }
+
+        if (response.pplayKnightCard != null) {
+            return (T) response.pplayKnightCard;
+        }
+
+        if (response.pRoadBuildingCardInfo != null) {
+            return (T) response.pRoadBuildingCardInfo;
+        }
+
+        logger.info("CANNOT READ INPUT");
+        System.out.println("CANNOT READ INPUT");
+        return null;
+
+    }
+
+    public String createString(Object fromObject) {
+        Gson gson = new GsonBuilder().create();
+        String response = gson.toJson(fromObject, fromObject.getClass());
+        return response;
+    }
 }
