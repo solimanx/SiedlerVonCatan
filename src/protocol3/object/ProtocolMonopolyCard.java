fd695388af94ea65c412a08cfc47940e08e305ae
matthias.moosburger <matthias.moosburger@d5b95618-209e-438a-8481-01c1a498ce88>
2016-06-08 10:12:14 +0000

    bugfix MonopolyCard missing "}"

--- src/protocol3/object/ProtocolMonopolyCard.java
+++ src/protocol3/object/ProtocolMonopolyCard.java
@@ -8,16 +8,17 @@ import protocol.object.ProtocolResource;
  * Created on 06.06.2016.
  */
 public class ProtocolMonopolyCard {
-	
+
 	@SerializedName("Rohstoffe")
 	ProtocolResource resource;
-	
+
 	public ProtocolMonopolyCard(ProtocolResource resource) {
 		this.resource = resource;
-		
+
 	}
 
 	public ProtocolResource getResource() {
 		return resource;
 
+	}
 }
