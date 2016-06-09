73c6e6e4ee283d75dad85d1b3acc8d2c99f72f19
roxana.shaikh <roxana.shaikh@d5b95618-209e-438a-8481-01c1a498ce88>
2016-06-09 13:26:07 +0000

    ProtocoloadBuildingCard edited

--- src/protocol3/object/ProtocolRoadBuildingCard.java
+++ src/protocol3/object/ProtocolRoadBuildingCard.java
@@ -8,30 +8,25 @@ import com.google.gson.annotations.Since;
  */
 @Since(0.3)
 public class ProtocolRoadBuildingCard {
-	
-	   @SerializedName("Straße 1")
-	    String road1_id;
-	    @SerializedName("Straße 2")
-	    String road2_id;
-	    @SerializedName("Spieler")
-	    int player_id;
 
-	    public ProtocolRoadBuildingCard(String road1_id, String road2_id, int player_id) {
-	        this.road1_id = road1_id;
-	        this.road2_id = road2_id;
-	        this.player_id = player_id;
-	    }
+	@SerializedName("Straße 1")
+	String road1_id;
+	@SerializedName("Straße 2")
+	String road2_id;
 
-	    public String getRoad1_id() {
+	public ProtocolRoadBuildingCard(String road1_id, String road2_id) {
+		this.road1_id = road1_id;
+		this.road2_id = road2_id;
 
-	        return road1_id;
-	    }
+	}
 
-	    public String getRoad2_id() {
-	        return road2_id;
-	    }
+	public String getRoad1_id() {
 
-	    public int getPlayer_id() {
-	        return player_id;
-	    }
+		return road1_id;
+	}
+
+	public String getRoad2_id() {
+		return road2_id;
+	}
+
 }
