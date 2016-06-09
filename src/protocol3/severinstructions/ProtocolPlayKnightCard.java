fa9c716e3cdf229e293fb140ba2c6111a854f0e4
amina.dacikj <amina.dacikj@d5b95618-209e-438a-8481-01c1a498ce88>
2016-06-09 13:50:52 +0000

    edit ProtocolPlayCardInfo

--- src/protocol3/severinstructions/ProtocolPlayKnightCard.java
+++ src/protocol3/severinstructions/ProtocolPlayKnightCard.java
@@ -15,26 +15,29 @@ import com.google.gson.annotations.Since;
 @Since(0.3)
 public class ProtocolPlayKnightCard {
 
-	@SerializedName("Straße 1")
-	String road1_id;
+    @SerializedName("Straße 1")
+    private String road1_id;
 
-	@SerializedName("Ziel")
-	int target;
+    @SerializedName("Ziel")
+    private int target;
+    @SerializedName("Spieler")
+    private int player_id;
 
-	public ProtocolPlayKnightCard(String road1_id, int target) {
+    public ProtocolPlayKnightCard(String road1_id, int target, int player_id) {
+        this.road1_id = road1_id;
+        this.target = target;
+        this.player_id = player_id;
+    }
 
-		this.road1_id = road1_id;
-		this.target = target;
-	}
+    public String getRoad1_id() {
+        return road1_id;
+    }
 
-	public String getRoad1_id() {
+    public int getTarget() {
+        return target;
+    }
 
-		return road1_id;
-	}
-
-	public int getTarget() {
-
-		return target;
-	}
-
+    public int getPlayer_id() {
+        return player_id;
+    }
 }
