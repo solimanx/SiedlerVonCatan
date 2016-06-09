f6c2e8e9358b42a68b6327a8b88a7c2b15908310
amina.dacikj <amina.dacikj@d5b95618-209e-438a-8481-01c1a498ce88>
2016-06-09 13:28:32 +0000

    edit ProtocolRoadBuildingCardInfo

--- src/protocol3/severinstructions/ProtocolRoadBuildingCardInfo.java
+++ src/protocol3/severinstructions/ProtocolRoadBuildingCardInfo.java
@@ -2,6 +2,7 @@ package protocol3.severinstructions;
 
 import com.google.gson.annotations.SerializedName;
 
+import com.google.gson.annotations.Since;
 import protocol3.object.ProtocolRoadBuildingCard;
 
 /**
@@ -12,19 +13,33 @@ import protocol3.object.ProtocolRoadBuildingCard;
  * </p>
  * Created on 06.06.2016.
  */
+@Since(0.3)
 public class ProtocolRoadBuildingCardInfo {
 
-    @SerializedName("Stra�e")
-    private ProtocolRoadBuildingCard road;
+    @SerializedName("Straße 1")
+    private ProtocolRoadBuildingCard road1_id;
 
-    public ProtocolRoadBuildingCardInfo (ProtocolRoadBuildingCard road) {
-        this.road = road;
+    @SerializedName("Straße 2")
+    private ProtocolRoadBuildingCard road2_id;
 
+@SerializedName("Spieler")
+    private int player_id;
+
+    public ProtocolRoadBuildingCardInfo(ProtocolRoadBuildingCard road1_id, ProtocolRoadBuildingCard road2_id, int player_id) {
+        this.road1_id = road1_id;
+        this.road2_id = road2_id;
+        this.player_id = player_id;
     }
 
-    public ProtocolRoadBuildingCard getRoadBuildingCard() {
-
-        return road;
+    public ProtocolRoadBuildingCard getRoad1_id() {
+        return road1_id;
     }
 
+    public ProtocolRoadBuildingCard getRoad2_id() {
+        return road2_id;
+    }
+
+    public int getPlayer_id() {
+        return player_id;
+    }
 }
