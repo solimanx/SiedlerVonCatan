648875a02c636d3fc8d051bc63d728f0dc393078
adam.mahmoud <adam.mahmoud@d5b95618-209e-438a-8481-01c1a498ce88>
2016-06-11 10:02:51 +0000

    Added value to PlayerState

--- src/enums/PlayerState.java
+++ src/enums/PlayerState.java
@@ -3,26 +3,41 @@ package enums;
 import com.google.gson.annotations.SerializedName;
 
 public enum PlayerState {
-	@SerializedName("Spiel starten") GAME_STARTING,
+	@SerializedName("Spiel starten") GAME_STARTING("Setting up"),
 
-	@SerializedName("Wartet auf Spielbeginn") WAITING_FOR_GAMESTART,
+	@SerializedName("Wartet auf Spielbeginn") WAITING_FOR_GAMESTART("Waiting for game begin"),
 
-	@SerializedName("Dorf bauen") BUILDING_VILLAGE,
+	@SerializedName("Dorf bauen") BUILDING_VILLAGE("Building settlement"),
 
-	@SerializedName("Straße bauen") BUILDING_STREET,
+	@SerializedName("Straße bauen") BUILDING_STREET("Building road"),
 
-	@SerializedName("Würfeln") DICEROLLING,
+	@SerializedName("Würfeln") DICEROLLING("Rolling dice"),
 
-	@SerializedName("Karten wegen Räuber abgeben") DISPENSE_CARDS_ROBBER_LOSS,
+	@SerializedName("Karten wegen Räuber abgeben") DISPENSE_CARDS_ROBBER_LOSS("Losing resources to robber"),
 
-	@SerializedName("Räuber versetzen") MOVE_ROBBER,
+	@SerializedName("Räuber versetzen") MOVE_ROBBER("Moving robber"),
 
-	@SerializedName("Handeln oder Bauen") TRADING_OR_BUILDING,
+	@SerializedName("Handeln oder Bauen") TRADING_OR_BUILDING("Trading/Building"),
 
-	@SerializedName("Warten") WAITING,
+	@SerializedName("Warten") WAITING("Waiting"),
 
-	@SerializedName("Verbindung verloren") CONNECTION_LOST,
+	@SerializedName("Verbindung verloren") CONNECTION_LOST("Disconnected"),
 
 	// OLD enums for debugging only
-	TRADING, OFFERING, PLAYING
+	TRADING(""), OFFERING(""), PLAYING("");
+	
+	private String value;
+	PlayerState(String value){
+		this.value = value;
+	}
+	
+	@Override
+	public String toString(){
+		return this.getValue();
+	}
+
+	private String getValue() {
+		
+		return value;
+	}
 }
