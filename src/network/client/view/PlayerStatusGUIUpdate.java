1a73c5827ce1f2905a2ce7c3a97118e2d8f501cd
matthias.moosburger <matthias.moosburger@d5b95618-209e-438a-8481-01c1a498ce88>
2016-06-11 16:22:12 +0000

    trying to solve FXthread problems

---
+++ src/network/client/view/PlayerStatusGUIUpdate.java
@@ -0,0 +1,34 @@
+package network.client.view;
+
+import enums.PlayerState;
+import network.client.controller.ViewController;
+
+public class PlayerStatusGUIUpdate implements Runnable {
+	
+
+	final int modelID;
+	final GameViewController gameViewController;
+	final int victoryPoints;
+	final PlayerState status;
+	final int[] resources;
+
+	public PlayerStatusGUIUpdate(int modelID, GameViewController gameViewController, int victoryPoints,
+			PlayerState status, int[] resources) {
+		super();
+		this.modelID = modelID;
+		this.gameViewController = gameViewController;
+		this.victoryPoints = victoryPoints;
+		this.status = status;
+		this.resources = resources;
+	}
+
+
+
+	@Override
+	public void run() {
+		gameViewController.setVictoryPoints(modelID, victoryPoints);
+		gameViewController.setResourceCards(modelID, resources);
+		gameViewController.setPlayerStatus(modelID, status);
+	}
+
+}
