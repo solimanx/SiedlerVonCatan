e9f3f46461b3fb8adef2b0f7249e3041682b21b5
amina.dacikj <amina.dacikj@d5b95618-209e-438a-8481-01c1a498ce88>
2016-06-13 18:30:21 +0000

    logging TestSpiral

--- src/model/TestSpiral.java
+++ src/model/TestSpiral.java
@@ -1,17 +1,22 @@
 package model;
 
 import java.util.ArrayList;
+
 import model.*;
 import model.objects.Field;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class TestSpiral {
+    private static Logger logger = LogManager.getLogger(TestSpiral.class.getName());
 
-	public static void main(String[] args) {
-		Board board = new Board();
-		String result = "";
-		result = HexService.getSpiral("N");
-		for (int i = 0; i < result.length(); i++) {
-			System.out.println(result.charAt(i));
-		}
-	}
+    public static void main(String[] args) {
+        Board board = new Board();
+        String result = "";
+        result = HexService.getSpiral("N");
+        for (int i = 0; i < result.length(); i++) {
+            System.out.println(result.charAt(i));
+            logger.info(result.charAt(i));
+        }
+    }
 }
