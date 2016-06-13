023beccd685730bfd8a4d6068d4b5a0fe1e0542d
amina.dacikj <amina.dacikj@d5b95618-209e-438a-8481-01c1a498ce88>
2016-06-13 21:54:52 +0000

    logging for catching an exception

--- src/network/client/client/Client.java
+++ src/network/client/client/Client.java
@@ -11,78 +11,80 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Client extends Thread {
-	static int connectionTry = 0;
-	private Socket socket = null;
-	private OutputStreamWriter writer = null;
-	private BufferedReader reader = null;
-	private int port = 8080;
-	private String serverHost = "localhost";
-	boolean scanning = true;
-	boolean connectionActive = false;
-	protected ClientInputHandler inputHandler;
-	private static Logger logger = LogManager.getLogger(Client.class.getName());
+    static int connectionTry = 0;
+    private Socket socket = null;
+    private OutputStreamWriter writer = null;
+    private BufferedReader reader = null;
+    private int port = 8080;
+    private String serverHost = "localhost";
+    boolean scanning = true;
+    boolean connectionActive = false;
+    protected ClientInputHandler inputHandler;
+    private static Logger logger = LogManager.getLogger(Client.class.getName());
 
-	public Client(ClientInputHandler inputHandler, String serverHost, int port) {
-		this.port = port;
-		this.serverHost = serverHost;
-		this.inputHandler = inputHandler;
-	}
+    public Client(ClientInputHandler inputHandler, String serverHost, int port) {
+        this.port = port;
+        this.serverHost = serverHost;
+        this.inputHandler = inputHandler;
+    }
 
-	public void setPort(int port) {
-		this.port = port;
-	}
+    public void setPort(int port) {
+        this.port = port;
+    }
 
-	@Override
-	public void run() {
-		while (scanning && connectionTry < 10) {
-			try {
-				socket = new Socket(serverHost, port);
-				writer = new OutputStreamWriter(socket.getOutputStream(), "UTF-8");
-				reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
-				scanning = false;
-				connectionActive = true;
-				System.out.println("Client connected to server.");
-				logger.info("Client connected to server");
-				runClient();
-			} catch (IOException e) {
-				System.out.println("Connection to server failed." + " Attempt:" + connectionTry + 1);
-				logger.debug("Connection to server failed." ," Attempt:" , connectionTry + 1);
-				connectionTry++;
-				try {
-					Thread.sleep(2000);
-				} catch (InterruptedException ie) {
-					logger.catching(Level.DEBUG,ie);
-					ie.printStackTrace();
-				}
-			}
-		}
-	}
+    @Override
+    public void run() {
+        while (scanning && connectionTry < 10) {
+            try {
+                socket = new Socket(serverHost, port);
+                writer = new OutputStreamWriter(socket.getOutputStream(), "UTF-8");
+                reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+                scanning = false;
+                connectionActive = true;
+                System.out.println("Client connected to server.");
+                logger.info("Client connected to server");
+                runClient();
+            } catch (IOException e) {
+                System.out.println("Connection to server failed." + " Attempt:" + connectionTry + 1);
+                logger.debug("Connection to server failed.", " Attempt:", connectionTry + 1);
+                connectionTry++;
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException ie) {
+                    logger.error("Interrupted Exception", ie);
+                    logger.catching(Level.ERROR, ie);
+                    ie.printStackTrace();
+                }
+            }
+        }
+    }
 
-	private void runClient() throws IOException {
-		String line;
-		while ((line = reader.readLine()) != null) {
-			System.out.println("Received from Server: " + line);
-			logger.debug("Received from Server: " , line);
-			inputHandler.sendToParser(line);
-			// redirect line to Networkcontroller
-		}
-	}
+    private void runClient() throws IOException {
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println("Received from Server: " + line);
+            logger.debug("Received from Server: ", line);
+            inputHandler.sendToParser(line);
+            // redirect line to Networkcontroller
+        }
+    }
 
-	public void write(String s) throws IOException {
-		System.out.println("Client sends to Server: " + s);
-		logger.debug("Client sends to Server: " , s);
-		writer.write(s + "\n");
-		writer.flush();
-	}
+    public void write(String s) throws IOException {
+        System.out.println("Client sends to Server: " + s);
+        logger.debug("Client sends to Server: ", s);
+        writer.write(s + "\n");
+        writer.flush();
+    }
 
-	public void stopClient() {
-		try {
-			socket.close();
-			connectionActive = false;
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			logger.warn("Input/Output Exception", e);
-			e.printStackTrace();
-		}
-	}
+    public void stopClient() {
+        try {
+            socket.close();
+            connectionActive = false;
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            logger.error("Input/Output Exception", e);
+            logger.catching(Level.ERROR, e);
+            e.printStackTrace();
+        }
+    }
 }
