e9cb9b3e70f44f2464203d06df789d1351fe1cb5
amina.dacikj <amina.dacikj@d5b95618-209e-438a-8481-01c1a498ce88>
2016-06-13 16:59:28 +0000

    logging for PlayerProfileController

--- src/application/lobby/PlayerProfileController.java
+++ src/application/lobby/PlayerProfileController.java
@@ -11,66 +11,71 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import network.client.controller.ViewController;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class PlayerProfileController implements Initializable {
+    private static Logger logger = LogManager.getLogger(PlayerProfileController.class.getName());
+    private ViewController viewController;
 
-	private ViewController viewController;
+    @FXML
+    private TextField playerName;
 
-	@FXML
-	private TextField playerName;
+    @FXML
+    private Button sendNameAndColor;
 
-	@FXML
-	private Button sendNameAndColor;
+    @FXML
+    private Button readyButton;
 
-	@FXML
-	private Button readyButton;
+    @FXML
+    private ChoiceBox<enums.Color> playerColor;
 
-	@FXML
-	private ChoiceBox<enums.Color> playerColor;
+    @FXML
+    private Label serverColorAnswer;
 
-	@FXML
-	private Label serverColorAnswer;
+    public void setServerColorAnswer(String server_response) {
+        serverColorAnswer.setText(server_response);
+    }
 
-	public void setServerColorAnswer(String server_response) {
-		serverColorAnswer.setText(server_response);
-	}
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        playerName.setPromptText("Name in Game (required)");
+        playerColor.getItems().addAll(enums.Color.BLUE, enums.Color.ORANGE, enums.Color.RED, enums.Color.WHITE);
+        readyButton.setDisable(true);
+    }
 
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		playerName.setPromptText("Name in Game (required)");
-		playerColor.getItems().addAll(enums.Color.BLUE, enums.Color.ORANGE, enums.Color.RED, enums.Color.WHITE);
-		readyButton.setDisable(true);
-	}
+    public void setViewController(ViewController viewController) {
+        this.viewController = viewController;
+    }
 
-	public void setViewController(ViewController viewController) {
-		this.viewController = viewController;
-	}
+    @FXML
+    private void handleSendButton() {
+        Color chosenColor = playerColor.getValue();
+        String name = playerName.getText();
+        serverColorAnswer.setText("");
+        if (name.equals("")) {
+            System.out.println("No name given.");
+            logger.info("No name given");
+        } else if (chosenColor == null) {
+            System.out.println("No color selected.");
+            logger.info("No color selected");
+        } else {
+            viewController.getClientController().sendPlayerProfile(name, chosenColor);
+            // TODO receive confirmation from server that color isnt taken
+            // FOR DEBUG ONLY ASSUME SERVER CONFIRMED
+            System.out.println("Profile: " + name + " " + chosenColor);
+            logger.debug("Profile", name, chosenColor);
+            readyButton.setDisable(false);
+        }
+    }
 
-	@FXML
-	private void handleSendButton() {
-		Color chosenColor = playerColor.getValue();
-		String name = playerName.getText();
-		serverColorAnswer.setText("");
-		if (name.equals("")) {
-			System.out.println("No name given.");
-		} else if (chosenColor == null) {
-			System.out.println("No color selected.");
-		} else {
-			viewController.getClientController().sendPlayerProfile(name, chosenColor);
-			// TODO receive confirmation from server that color isnt taken
-			// FOR DEBUG ONLY ASSUME SERVER CONFIRMED
-			System.out.println("Profile: " + name + " " + chosenColor);
-			readyButton.setDisable(false);
-		}
-	}
+    @FXML
+    private void handleReadyButton() {
+        viewController.getClientController().sendReady();
+    }
 
-	@FXML
-	private void handleReadyButton() {
-		viewController.getClientController().sendReady();
-	}
-
-	// @FXML
-	// public void setServerColorText(String s) {
-	// serverColorAnswer.setText(s);
-	// }
+    // @FXML
+    // public void setServerColorText(String s) {
+    // serverColorAnswer.setText(s);
+    // }
 }
