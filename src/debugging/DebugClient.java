d0c781c22f9eb39d8dda35a85e66a659b43179eb
amina.dacikj <amina.dacikj@d5b95618-209e-438a-8481-01c1a498ce88>
2016-06-13 17:11:09 +0000

    logging DebugClient

--- src/debugging/DebugClient.java
+++ src/debugging/DebugClient.java
@@ -6,38 +6,46 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * To "illegaly" test sending JSONs, and figuring out how the test server
  * responds.
  */
-public class DebugClient extends Application{
-	DebugClientController dcc;
-	public DebugClient(Stage primaryStage) {
-		System.out.println("Running debugging/testing mode");
-		try {
-			start(primaryStage);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+public class DebugClient extends Application {
+    private static Logger logger = LogManager.getLogger(DebugClient.class.getName());
+    DebugClientController dcc;
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		FXMLLoader loader = new FXMLLoader();
-		Parent root = loader.load(getClass().getResource("/debugging/DebugClientFXML.fxml").openStream());
-		Scene scene = new Scene(root);
-		dcc = (DebugClientController) loader.getController();
-		primaryStage.setScene(scene);
-		primaryStage.sizeToScene();
-		//
-		primaryStage.setTitle("Debug Client");
-		primaryStage.setResizable(false);
-		primaryStage.setOnCloseRequest(e -> System.exit(0));
-		// primaryStage.initStyle(StageStyle.UTILITY);
-		//
-		primaryStage.show();
-		
-	}
+    public DebugClient(Stage primaryStage) {
+        System.out.println("Running debugging/testing mode");
+        logger.debug("Running debugging/testing mode");
+
+        try {
+            start(primaryStage);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            logger.catching(Level.DEBUG, e);
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        Parent root = loader.load(getClass().getResource("/debugging/DebugClientFXML.fxml").openStream());
+        Scene scene = new Scene(root);
+        dcc = (DebugClientController) loader.getController();
+        primaryStage.setScene(scene);
+        primaryStage.sizeToScene();
+        //
+        primaryStage.setTitle("Debug Client");
+        primaryStage.setResizable(false);
+        primaryStage.setOnCloseRequest(e -> System.exit(0));
+        // primaryStage.initStyle(StageStyle.UTILITY);
+        //
+        primaryStage.show();
+
+    }
 }
