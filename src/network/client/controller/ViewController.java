73e5d92284d9fd83018d25dbe180d9224d143250
matthias.moosburger <matthias.moosburger@d5b95618-209e-438a-8481-01c1a498ce88>
2016-06-14 14:29:03 +0000

    fullscreen

--- src/network/client/controller/ViewController.java
+++ src/network/client/controller/ViewController.java
@@ -135,11 +135,11 @@ public class ViewController {
             Scene scene = new Scene(root);
             scene.getStylesheets().add(getClass().getResource("/network/client/view/application.css").toExternalForm());
             primaryStage.setScene(scene);
-//			Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
-//	        primaryStage.setX(primaryScreenBounds.getMinX());
-//	        primaryStage.setY(primaryScreenBounds.getMinY());
-//	        primaryStage.setWidth(primaryScreenBounds.getWidth());
-//	        primaryStage.setHeight(primaryScreenBounds.getHeight());
+			Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+	        primaryStage.setX(primaryScreenBounds.getMinX());
+	        primaryStage.setY(primaryScreenBounds.getMinY());
+	        primaryStage.setWidth(primaryScreenBounds.getWidth());
+	        primaryStage.setHeight(primaryScreenBounds.getHeight());
             //primaryStage.setFullScreen(false);
             gameViewController = (GameViewController) loader.getController();
             gameViewController.setViewController(this);
