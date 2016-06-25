63619547502967e950176faf22edaa13d4be4e07
adam.mahmoud <adam.mahmoud@d5b95618-209e-438a-8481-01c1a498ce88>
2016-06-25 11:06:57 +0000

    Opponent agent

--- src/ai/agents/OpponentAgent.java
+++ src/ai/agents/OpponentAgent.java
@@ -8,6 +8,7 @@ import enums.ResourceType;
 import model.Board;
 import model.objects.Corner;
 import model.objects.Field;
+import model.objects.PlayerModel;
 import settings.DefaultSettings;
 
 /**
@@ -16,20 +17,30 @@ import settings.DefaultSettings;
  */
 public class OpponentAgent {
 
-	private int[][] opponentsRessources;
+	private ArrayList<PlayerModel> opponents = new ArrayList<PlayerModel>();
+	
+//	private int[][] opponentsRessources;
 	// {WOOD, CLAY, ORE, SHEEP, CORN, unknown, missing} missing is positive>0
 	// after robber loss
-	private int[] initialRessources = { 0, 0, 0, 0, 0, 0, 0 };
-	private int amountPlayer;
+//	private int[] initialRessources = { 0, 0, 0, 0, 0, 0, 0 };
+//	private int amountPlayer;
 	private Board board;
 
-	public OpponentAgent(AdvancedAI aai) {
-		board = aai.getGl().getBoard();
-		amountPlayer = board.getAmountPlayers();
-		opponentsRessources = new int[amountPlayer][7];
-		for (int i = 0; i < amountPlayer; i++) {
-			opponentsRessources[i] = initialRessources;
+	public PlayerModel getOpponent(Integer ID){
+		for(int i=0; i<opponents.size(); i++){
+			if(opponents.get(i).getID() == ID){
+				return opponents.get(i);
+			}
 		}
+		throw new IllegalArgumentException("ID "+ID+" doesn't exist");
+	}
+	public OpponentAgent(AdvancedAI aai) {
+//		board = aai.getGl().getBoard();
+//		amountPlayer = board.getAmountPlayers();
+//		opponentsRessources = new int[amountPlayer][7];
+//		for (int i = 0; i < amountPlayer; i++) {
+//			opponentsRessources[i] = initialRessources;
+//		}
 	}
 
 	// inputhandler iunpur an diese methode
@@ -38,7 +49,8 @@ public class OpponentAgent {
 	}
 
 	public void robberLossEnemy(int playerID){
-		opponentsRessources[playerID][7] = opponentsRessources[playerID][7] -1;
+		getOpponent(playerID).decrementHiddenResources(1);
+		//		opponentsRessources[playerID][7] = opponentsRessources[playerID][7] -1;
 	}
 
 	public void buildingCostEnemy(int playerID, int[] costs){
@@ -49,27 +61,28 @@ public class OpponentAgent {
 	}
 	
 	private void decrementOpponentsRessources(int playerID, int[] costs){
-		for(int i = 0; i<5; i++){
-			if(costs[i] != 0){
-				if(opponentsRessources[playerID][i]<costs[i]){
-					for(int j = 0; j<costs[i]; j++){
-						if(opponentsRessources[playerID][i] != 0){
-							opponentsRessources[playerID][i]--;
-						}
-						else{
-							if(opponentsRessources[playerID][6] != 0){
-								opponentsRessources[playerID][6]--;
-							}
-							else{
-								throw new IllegalStateException("Error in calculation of enemy resources");
-							}
-						}
-					}
-				} else {
-					opponentsRessources[playerID][i] = opponentsRessources[playerID][i] - costs[i];
-				}
-			}
-		}
+		getOpponent(playerID).decrementResources(costs);
+//		for(int i = 0; i<5; i++){
+//			if(costs[i] != 0){
+//				if(opponentsRessources[playerID][i]<costs[i]){
+//					for(int j = 0; j<costs[i]; j++){
+//						if(opponentsRessources[playerID][i] != 0){
+//							opponentsRessources[playerID][i]--;
+//						}
+//						else{
+//							if(opponentsRessources[playerID][6] != 0){
+//								opponentsRessources[playerID][6]--;
+//							}
+//							else{
+//								throw new IllegalStateException("Error in calculation of enemy resources");
+//							}
+//						}
+//					}
+//				} else {
+//					opponentsRessources[playerID][i] = opponentsRessources[playerID][i] - costs[i];
+//				}
+//			}
+//		}
 	}
 
 	public void tradingEnemy(int playerID){
