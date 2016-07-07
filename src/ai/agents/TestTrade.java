22e1be7af0cb0bf8f9dbe0cda746dbed39da953b
j.reff <j.reff@d5b95618-209e-438a-8481-01c1a498ce88>
2016-07-07 12:59:03 +0000

    testTrade auskommentiert :)

--- src/ai/agents/TestTrade.java
+++ src/ai/agents/TestTrade.java
@@ -7,26 +7,26 @@ import ai.AdvancedAI;
 public class TestTrade {
 
 	public static void main(String[] args){
-		// Amount of Landscape Resource Cards: {WOOD, CLAY, ORE, SHEEP, CORN}
-		//	CITY_BUILD_COST = { 0, 0, 3, 0, 2 };
-		TradeAgent ta = new TradeAgent(null, null);
-		//ta.setHarbor();
-		int[] resources = { 4, 4, 1, 0, 2};
-		System.out.println(ta.isBuildableAfterTrade(2, resources));
-		ArrayList<TradeOffer> to = new ArrayList<TradeOffer>();
-		if(ta.isBuildableAfterTrade(2, resources)){
-			to = ta.tradesForBuilding(2, resources);
-		}
-		for(int i= 0; i<to.size(); i++){
-			for(int j = 0; j<to.get(i).getDemand().length; j++){
-				System.out.print(to.get(i).getDemand()[j]);
-			}
-			System.out.print("  ");
-			for(int j = 0; j<to.get(i).getOffer().length; j++){
-				System.out.print(to.get(i).getOffer()[j]);
-			}
-			System.out.println("");
-		}
+//		// Amount of Landscape Resource Cards: {WOOD, CLAY, ORE, SHEEP, CORN}
+//		//	CITY_BUILD_COST = { 0, 0, 3, 0, 2 };
+//		TradeAgent ta = new TradeAgent(null, null);
+//		//ta.setHarbor();
+//		int[] resources = { 4, 4, 1, 0, 2};
+//		System.out.println(ta.isBuildableAfterTrade(2, resources));
+//		ArrayList<TradeOffer> to = new ArrayList<TradeOffer>();
+//		if(ta.isBuildableAfterTrade(2, resources)){
+//			to = ta.tradesForBuilding(2, resources);
+//		}
+//		for(int i= 0; i<to.size(); i++){
+//			for(int j = 0; j<to.get(i).getDemand().length; j++){
+//				System.out.print(to.get(i).getDemand()[j]);
+//			}
+//			System.out.print("  ");
+//			for(int j = 0; j<to.get(i).getOffer().length; j++){
+//				System.out.print(to.get(i).getOffer()[j]);
+//			}
+//			System.out.println("");
+//		}
 
 	}
 
